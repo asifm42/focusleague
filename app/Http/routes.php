df4216cd669048bcc0681eab0b95bb4b51beb604
Asif Mohammed <asifm42@gmail.com>
2016-02-22 15:13:32 -0600

    adding temp faq page

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/faq', function () {
+    return view('faq');
+});
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
---
+++ resources/views/faq.blade.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>FOCUS League</title>
+
+    <link rel="stylesheet" type="text/css" href="{{ url('assets/bootstrap/dist/css/bootstrap.min.css') }}">
+ {{--    <link rel="stylesheet" type="text/css" href="{{ url('assets/css/font-awesome.min.css') }}"> --}}
+    <link rel="stylesheet" type="text/css" href="{{ url('assets/css/paper.bootstrap.min.css') }}">
+{{--    <link rel="stylesheet" type="text/css" href="{{ url('assets/css/site.min.css') }}"> --}}
+
+        <style>
+            html, body {
+                height: 100%;
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                display: table;
+            }
+
+            .container {
+                margin: 4px;
+                padding: 2px;
+                /*text-align: center;*/
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 96px;
+                text-align: center;
+            }
+
+            .subtitle {
+                font-size: 42px;
+                text-align: center;
+            }
+
+            .nowrap {
+                white-space: nowrap;
+            }
+
+            .panel {
+                margin:10px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title nowrap">F.O.C.U.S. League</div>
+                <div class="subtitle nowrap">Frequently Asked Questions</div>
+                <div class="panel panel-default">
+                    <div class="panel-heading">What is Ultimate?</div>
+                    <div class="panel-body">
+                        <p style="font-style:italic">Ultimate, originally known as ultimate frisbee, is a non-contact team field sport played with a flying disc. Points are scored by passing the disc to a teammate in the opposing end zone. Other basic rules are that players must not take steps while holding the disc, and interceptions, incomplete passes, and passes out of bounds are turnovers. Rain, wind, or occasionally other adversities can make for a testing match with rapid turnovers, heightening the pressure of play.</p>
+                        <p>- <a href="https://en.wikipedia.org/wiki/Ultimate_(sport)">Wikipedia Ultimate (sport)</a></p>
+                    </div>
+                </div>
+                <div class="panel panel-default">
+                    <div class="panel-heading">What is the FOCUS League?</div>
+                    <div class="panel-body">
+                        <p>The FOCUS League is a competitive Ultimate league in Houston intended for players with intermediate to advanced skills in the sport of Ultimate. The purpose of this league is to structure would-be pick-up games into a series of 4 week cycles and increase the availability of competitive Ultimate in Houston. Registered players will be divided into X number of teams and remain together for a 4 week cycle. Each team will have a team captain(s) who will lead the team in developing the team offense and defense (i.e., teams will call offensive plays, call defenses with a marking direction, work on zones, crumbles, and any other strategic points of interest of the team)</p>
+                    </div>
+                </div>
+                <div class="panel panel-default">
+                    <div class="panel-heading">What is the format?</div>
+                    <div class="panel-body">
+                        <p>The ideal format is to have one Mens 7v7 game and one Mixed (4/3 or 5/2) or Womens 7v7 game. However, the format for each cycle will be determined by the number and gender ratio of registered players. It could be two Mens 5v5 games and one Womens 5v5 game. It could be two 7v7 Mixed games. It could be one Mens 5v5 game, one Mixed 5v5 game and one Womens 5v5 game. It could be two Mens 7v7 games. It could be...you get the idea.</p>
+                    </div>
+                </div>
+                <div class="panel panel-default">
+                    <div class="panel-heading">What is the schedule?</div>
+                    <div class="panel-body">
+                        <p>Games will be hosted at the Houston Sports Park on Tuesday nights, 8p-10p. The number of games each night will be determined by the format.</p>
+                    </div>
+                </div>
+                <div class="panel panel-default">
+                    <div class="panel-heading">Who can play?</div>
+                    <div class="panel-body">
+                        <p>The FOCUS league is open to competitive male and female players who want to improve their skills through playing high level Ultimate in the framework of a team by practicing the same skills for 4 weeks with the same group of similarly minded players. We may cap the number of signups on a given cycle depending on availability.</p>
+                    </div>
+                </div>
+                <div class="panel panel-default">
+                    <div class="panel-heading">What if I am a beginner?</div>
+                    <div class="panel-body">
+                        <p>You are still able to play as The FOCUS league does not exclude anyone (unless registration is capped due to capacity). However, the FOCUS league is intended for the experienced player. If you do not understand terms/concepts like force, flick, backhand, hammer, scoober, huck, cut, vertical stack offense, horizontal offense, man defense, zone defense, zone crumble, zone cup, foul, pick, poach, etc, as they relate to Ultimate then you may not find the FOCUS league an enjoyable experience. If you cannot throw a 15 yard leading flick and backhand with accuracy to a receiver then you may not find the FOCUS league an enjoyable experience.</p>
+
+                        <p>If you feel that the FOCUS League is not for you, the <a href="http://www.houstonultimate.org">Houston Ultimate Community (HUC)</a> hosts a variety of beginner/recreational leagues and tournaments.</p>
+                    </div>
+                </div>
+                <div class="panel panel-default">
+                    <div class="panel-heading">Why are you charging money?</div>
+                    <div class="panel-body">
+                        <p>A lot of time, effort and resources are needed into producing this experience. Additionally, the Houston Sports Park is the premier sports park in Houston with professional sports field maintenance and their field rental prices reflect that.</p>
+                    </div>
+                </div>
+                <div class="panel panel-default">
+                    <div class="panel-heading">What does FOCUS stand for?</div>
+                    <div class="panel-body">
+                        <p><strong>F</strong>ostering <strong>O</strong>rganized <strong>C</strong>ompetitive <strong>U</strong>ltimate <strong>S</strong>eries</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
