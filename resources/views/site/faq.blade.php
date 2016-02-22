0692b58e8b849419a0e92289e2199591ade8fe9d
Asif Mohammed <asifm42@gmail.com>
2016-02-22 17:27:05 -0600

    updated views

--- resources/views/site/faq.blade.php
+++ resources/views/site/faq.blade.php
@@ -1,19 +1,13 @@
 @extends('layouts.default')
 @section('title','FOCUS League – FAQ')
 @section('styles')
-    <style>
-            .subtitle {
-                font-size: 42px;
-                text-align: center;
-                white-space: nowrap;
-            }
-        </style>
+
 @stop
 @section('content')
     <div class="container-fluid">
         <div class="row">
             <div class="col-xs-12 col-md-10 col-md-offset-1">
-                <div class="subtitle text-center">Frequently Asked Questions</div>
+                <h3 class="text-center">Frequently Asked Questions</h3>
                 <div class="panel panel-default">
                     <div class="panel-heading">What is Ultimate?</div>
                     <div class="panel-body">
