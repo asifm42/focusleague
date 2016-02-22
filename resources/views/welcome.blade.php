d0684c9ea2f39f25807995401904059d13d1e084
Asif Mohammed <asifm42@gmail.com>
2016-02-22 11:16:00 -0600

    updated welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,8 +5,13 @@
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
+    <link rel="stylesheet" type="text/css" href="{{ url('assets/bootstrap/dist/css/bootstrap.min.css') }}">
+ {{--    <link rel="stylesheet" type="text/css" href="{{ url('assets/css/font-awesome.min.css') }}"> --}}
+    <link rel="stylesheet" type="text/css" href="{{ url('assets/css/paper.bootstrap.min.css') }}">
+{{--    <link rel="stylesheet" type="text/css" href="{{ url('assets/css/site.min.css') }}"> --}}
+
         <style>
-            html, body {
+/*            html, body {
                 height: 100%;
             }
 
@@ -17,11 +22,11 @@
                 display: table;
                 font-weight: 100;
                 font-family: 'Lato';
-            }
+            }*/
 
             .container {
                 text-align: center;
-                display: table-cell;
+                /*display: table-cell;*/
                 vertical-align: middle;
             }
 
@@ -37,6 +42,10 @@
             .subtitle {
                 font-size: 48px;
             }
+
+            .jumbotron {
+                font-weight:350;
+            }
         </style>
     </head>
     <body>
@@ -44,6 +53,11 @@
             <div class="content">
                 <div class="title">F.O.C.U.S. League</div>
                 <div class="subtitle"><strong>F</strong>ocused <strong>O</strong>rganized <strong>C</strong>ompetitive <strong>U</strong>ltimate <strong>S</strong>eries</div>
+                <div><h5>Every Tuesday 8p-10p starting March 15 at the Houston Sports Park</h5></div>
+                <div class="jumbotron">
+                    <h1>The Mission</h1>
+                    <p>To structure would-be pickup games into a league and increase the availability of competitive Ultimate in Houston.</p>
+                </div>
             </div>
         </div>
     </body>
