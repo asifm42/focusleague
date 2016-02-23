ea88610552c4a223ea6c05aa43d9276a3b25f325
Asif Mohammed <asifm42@gmail.com>
2016-02-23 16:42:13 -0600

    updated views

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -6,24 +6,38 @@
 
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 
+
+{{-- <link rel="stylesheet" type="text/css" href="{{ url('assets/bootstrap/dist/css/bootstrap.min.css') }}"> --}}
     <link rel="stylesheet" type="text/css" href="{{ url('assets/css/paper.bootstrap.min.css') }}">
-
-
+{{-- <link rel="stylesheet" type="text/css" href="{{ url('assets/css/font-awesome.min.css') }}"> --}}
+{{-- <link rel="stylesheet" type="text/css" href="{{ url('assets/css/site.min.css') }}"> --}}
+    @yield('styles')
 
 </head>
 <body id="@yield('bodyID')" class="@yield('class')">
 
+@include('navbars.default')
 
-
+@include('flash::message')
 
 @yield('content')
 
+@include('layouts.footer')
 
-
-
+{{-- <script type='text/javascript' src="{{ url('assets/js/jquery.min.js') }}"></script> --}}
 <script type='text/javascript' src="https://code.jquery.com/jquery-2.2.0.min.js"></script>
 <script type='text/javascript' src="{{ url('assets/bootstrap/dist/js/bootstrap.min.js') }}"></script>
 
+<script>
+    $(document).ready( function () {
+        // For popovers on the navbar
+        // $('[data-toggle="popover"]').popover();
+    })
+</script>
+
+@yield('scripts')
+
+{{-- @include('google.analytics') --}}
 
 </body>
 </html>
--- resources/views/site/welcome.blade.php
+++ resources/views/site/welcome.blade.php
@@ -1,8 +1,32 @@
 @extends('layouts.default')
 @section('title','FOCUS League – Welcome')
+@section('styles')
 
-
+@stop
 
 @section('content')
-{{ "hello world " }}
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <div class="text-center hidden-md hidden-lg">
+                    <h3>FOCUS League</h3>
+                    <h5><strong>F</strong>ostering <strong>O</strong>rganized <strong>C</strong>ompetitive <strong>U</strong>ltimate <strong>S</strong>eries</h5>
+                    <h6>Every Tuesday, 8p-10p, starting March 15 at the Houston Sports Park</h6>
+                    <div class="jumbotron">
+                        <h4>The Mission</h4>
+                        <p>To structure would-be pickup games into a league and increase the availability of competitive Ultimate in Houston.</p>
+                    </div>
+                </div>
+                <div class="text-center hidden-xs hidden-sm">
+                    <h1>FOCUS League</h1>
+                    <h3><strong>F</strong>ostering <strong>O</strong>rganized <strong>C</strong>ompetitive <strong>U</strong>ltimate <strong>S</strong>eries</h3>
+                    <h5>Every Tuesday, 8p-10p, starting March 15 at the Houston Sports Park</h5>
+                    <div class="jumbotron">
+                        <h4>The Mission</h4>
+                        <p>To structure would-be pickup games into a league and increase the availability of competitive Ultimate in Houston.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @stop
