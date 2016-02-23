333ca790ec01ba9b50fa0130c0720cef44aaddc8
Asif Mohammed <asifm42@gmail.com>
2016-02-23 14:16:34 -0600

    updated views

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="container-fluid" style="border-top:1px #eee solid; padding-top:4px">
         <div class="page-footer text-muted">
             <div class="visible-xs-block">
-                <!-- &copy; {{ date("Y") }} <a href="http://focusleague.com">FOCUS League</a> -->
+                {{-- &copy; {{ date("Y") }} <a href="http://focusleague.com">FOCUS League</a> --}}
                 <p><strong>FOCUS League</strong> &copy; {{ date("Y") }} is brought to you by Asif Mohammed with the help of Matt Lyle and inspiration from Sean McCall's CSS 2002-2009<br />
                 <a href="http://focusleague.com">focusleague.com</a> developed by Asif Mohammed<br />
                 <a href="http://focusleague.com">focusleague.com</a> designed by Asif Mohammed &amp; Nicky Carranza</p>
--- resources/views/navbars/default.blade.php
+++ resources/views/navbars/default.blade.php
@@ -19,14 +19,14 @@
                 <li class={{ Active::pattern(['/'], 'active') }}><a href="{{ route('site.home') }}">Home</a></li>
                 <li class={{ Active::pattern(['news'], 'active') }}><a href="{{ route('site.news') }}">News</a></li>
                 <li class={{ Active::pattern(['faq'], 'active') }}><a href="{{ route('site.faq') }}">FAQ</a></li>
-                <!-- <li class="visible-xs-block visible-sm-block{{ Active::pattern(['signin'], ' active') }}"><a href="{{ url('signin') }}">Sign in</a></li> -->
-                <!-- <li class="visible-xs-block visible-sm-block{{ Active::pattern(['signup'], ' active') }}"><a href="{!! url('users/register') !!}">Sign Up</a></li> -->
+                {{-- <li class="visible-xs-block visible-sm-block{{ Active::pattern(['signin'], ' active') }}"><a href="{{ url('signin') }}">Sign in</a></li> --}}
+                {{-- <li class="visible-xs-block visible-sm-block{{ Active::pattern(['signup'], ' active') }}"><a href="{!! url('users/register') !!}">Sign Up</a></li> --}}
             </ul>
 
             <ul class="nav navbar-nav navbar-right hidden-xs hidden-sm">
 
-                 <!-- <li><a href="{{ url('signin') }}" class="">Sign In</a></li> -->
-                <!-- <li><a href="{!! url('users/register') !!}" class="">Sign Up</a></li> -->
+                 {{-- <li><a href="{{ url('signin') }}" class="">Sign In</a></li> --}}
+                {{-- <li><a href="{!! url('users/register') !!}" class="">Sign Up</a></li> --}}
 
             </ul>
 
