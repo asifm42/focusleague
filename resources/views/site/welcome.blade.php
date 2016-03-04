8cd5ade1d6832a4e4206dc057d3144cb64294fe5
Asif Mohammed <asifm42@gmail.com>
2016-03-03 23:57:28 -0600

    non bold focus meaning

--- resources/views/site/welcome.blade.php
+++ resources/views/site/welcome.blade.php
@@ -9,8 +9,9 @@
         <div class="row">
             <div class="col-xs-12">
                 <div class="text-center hidden-md hidden-lg">
-                    <h3>FOCUS League</h3>
-                    <h5><strong>F</strong>ostering <strong>O</strong>rganized <strong>C</strong>ompetitive <strong>U</strong>ltimate <strong>S</strong>eries</h5>
+                    <h3 style="color:000; font-weight:800;">FOCUS League</h3>
+                    <h5>Fostering Organized Competitive Ultimate Series</h5>
+                    <!-- <h5><strong>F</strong>ostering <strong>O</strong>rganized <strong>C</strong>ompetitive <strong>U</strong>ltimate <strong>S</strong>eries</h5> -->
                     <h6>Every Tuesday, 8p-10p, starting March 15 at the Houston Sports Park</h6>
                     <div class="jumbotron">
                         <h4>The Mission</h4>
@@ -18,8 +19,9 @@
                     </div>
                 </div>
                 <div class="text-center hidden-xs hidden-sm">
-                    <h1>FOCUS League</h1>
-                    <h3><strong>F</strong>ostering <strong>O</strong>rganized <strong>C</strong>ompetitive <strong>U</strong>ltimate <strong>S</strong>eries</h3>
+                    <h1 style="color:000; font-weight:800;">FOCUS League</h1>
+                    <!-- <h3><strong>F</strong>ostering <strong>O</strong>rganized <strong>C</strong>ompetitive <strong>U</strong>ltimate <strong>S</strong>eries</h3> -->
+                    <h3>Fostering Organized Competitive Ultimate Series</h3>
                     <h5>Every Tuesday, 8p-10p, starting March 15 at the Houston Sports Park</h5>
                     <div class="jumbotron">
                         <h4>The Mission</h4>
