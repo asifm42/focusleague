3e3c3a9bab60ede0d2be58dfb5afd995e4f877e2
Asif Mohammed <asifm42@gmail.com>
2016-03-03 21:15:11 -0600

    updated sessions controller

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -6,9 +6,18 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\SignInRequest;
+use App\Factories\SessionFactory;
+use Debugbar;
 
 class SessionsController extends Controller
 {
+    protected $sessionFactory;
+
+    public function __construct(SessionFactory $sessionFactory)
+    {
+        $this->sessionFactory = $sessionFactory;
+    }
 
     /**
      * Show the Sign In Form.
@@ -17,6 +26,41 @@ class SessionsController extends Controller
      */
     public function create(Request $request)
     {
-        return view('auth.signIn');
+        return view('auth.user.signIn');
+    }
+
+    public function signIn(SignInRequest $request)
+    {
+        $data = [];
+        $data['email']          =  $request->get('email');
+        $data['password']       =  $request->get('password');
+        $data['rememberMe']     = (($request->get('remember_me') === 'true') ? true : false);
+        $data['ip_address']     = $request->ip();
+
+        $session = $this->sessionFactory->make($data);
+
+        if ($session) {
+            return redirect()->intended('profile');
+        }
+
+        // Set up error flash message
+        flash()->error('<strong>Try again!</strong> Your email or password was incorrect.');
+
+        // redirect to login form. Don't need to send validation errors.
+        return redirect()->route('sessions.create')->withInput();
+    }
+
+    public function signOut()
+    {
+        if (! auth()->check()) {
+            return redirect(route('sessions.signin'));
+        }
+
+        auth()->logout();
+
+        // Set up success flash message
+        flash()->success('<strong>Success!</strong> You have signed out. Have a nice day!');
+
+        return redirect()->route('sessions.create');
     }
 }
