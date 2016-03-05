3f6bb2cdf1ce16a807cbfe17166a66521e86c82a
Asif Mohammed <asifm42@gmail.com>
2016-03-05 11:57:08 -0600

    updated faq

--- resources/views/site/faq.blade.php
+++ resources/views/site/faq.blade.php
@@ -15,16 +15,7 @@
         <div class="row">
             <div class="col-xs-12 col-md-10 col-md-offset-1">
                 <div class="panel panel-default">
-                    <div class="panel-heading">What is Ultimate?</div>
-                    <div class="panel-body">
-                        <blockquote>
-                            <p>Ultimate, originally known as ultimate frisbee, is a non-contact team field sport played with a flying disc. Points are scored by passing the disc to a teammate in the opposing end zone. Other basic rules are that players must not take steps while holding the disc, and interceptions, incomplete passes, and passes out of bounds are turnovers. Rain, wind, or occasionally other adversities can make for a testing match with rapid turnovers, heightening the pressure of play.</p>
-                            <footer><a href="https://en.wikipedia.org/wiki/Ultimate_(sport)">Wikipedia <cite title="Ultimate (sport)">Ultimate (sport)<cite></a></footer>
-                        </blockquote>
-                    </div>
-                </div>
-                <div class="panel panel-default">
-                    <div class="panel-heading">What is the FOCUS League?</div>
+                    <div class="panel-heading"><h4 class="panel-title">What is the FOCUS League?</h4></div>
                     <div class="panel-body">
                         <p>The FOCUS League is a competitive Ultimate league in Houston intended for players with intermediate to advanced skills in the sport of Ultimate. The purpose of this league is to structure would-be pick-up games into a series of 4 week cycles and increase the availability of competitive Ultimate in Houston. Registered players will be divided into X number of teams and remain together for a 4 week cycle. Each team will have a team captain(s) who will lead the team in developing the team offense and defense (i.e., teams will call offensive plays, call defenses with a marking direction, work on zones, crumbles, and any other strategic points of interest of the team).</p>
                     </div>
@@ -33,6 +24,12 @@
                     <div class="panel-heading">What is the format?</div>
                     <div class="panel-body">
                         <p>The ideal format is to have one Mens 7v7 game and one Mixed (4/3 or 5/2) or Womens 7v7 game. However, the format for each cycle will be determined by the number and gender ratio of registered players. It could be two Mens 5v5 games and one Womens 5v5 game. It could be two 7v7 Mixed games. It could be one Mens 5v5 game, one Mixed 5v5 game and one Womens 5v5 game. It could be two Mens 7v7 games. It could be...you get the idea.</p>
+                    </div>
+                </div>
+                <div class="panel panel-default">
+                    <div class="panel-heading">What if I don’t want to play in a certain division (i.e mens, womens, mixed)?</div>
+                    <div class="panel-body">
+                        <p>When signing up for a cycle, you will be able to indicate your first and second preference for a division. By leaving the second one blank, you will be indicting that you would prefer to sit out the cycle if there are not enough signups to support a game for your division.</p>
                     </div>
                 </div>
                 <div class="panel panel-default">
@@ -56,6 +53,18 @@
                     </div>
                 </div>
                 <div class="panel panel-default">
+                    <div class="panel-heading">How do I sign up?</div>
+                    <div class="panel-body">
+                        <p>You will need a player account and each cycle will have its own registration form online.</p>
+                    </div>
+                </div>
+                <div class="panel panel-default">
+                    <div class="panel-heading">When will sign ups open?</div>
+                    <div class="panel-body">
+                        <p>Sign ups for player accounts and the first cycle will open on March 9th. After that, each cycle sign up will open 6-7 days prior to the first day of that cycle. </p>
+                    </div>
+                </div>
+                <div class="panel panel-default">
                     <div class="panel-heading">How much does it cost?</div>
                     <div class="panel-body">
                         <p>Subs - $10/week</p>
@@ -67,28 +76,46 @@
                 <div class="panel panel-default">
                     <div class="panel-heading">How do I make a payment?</div>
                     <div class="panel-body">
-                        <p>Paypal, Chase Quickpay, Square Cash, Check or Cash</p>
-                        <p>More TBD</p>
+                        <p>We will be accepting Paypal, Chase Quickpay, Square Cash, Check or cash. More details to follow soon.</p>
                     </div>
                 </div>
                 <div class="panel panel-default">
                     <div class="panel-heading">Why are you charging money?</div>
                     <div class="panel-body">
-                        <p>A lot of time, effort and resources are required for producing this league. Additionally, the Houston Sports Park is the premier sports park in Houston with professional sports field maintenance and their field rental prices reflect that.</p>
+                        <p>Most of the funds go towards securing fields at the Houston Sports Park which is the premier sports park in Houston with professional sports field maintenance and their field rental prices reflect that. The remaining funds go towards administrative costs.</p>
                     </div>
                 </div>
                 <div class="panel panel-default">
-                    <div class="panel-heading">What are the captain's duties and benefits?</div>
+                    <div class="panel-heading">What are the team captain's duties?</div>
                     <div class="panel-body">
-                        <p>TBD</p>
-                        <p>Must be able to sign up for all 4 weeks</p>
-                        <p>Get a 30% discount on cycle fees</p>
+                        <p>Each team will have a team captain(s) who will lead the team in developing the team offense and defense (i.e., teams will call offensive plays, call defenses with a marking direction, work on zones, crumbles, and any other strategic points of interest of the team). They will also be involved in balancing the teams for the respective cycle and will also serve as a liaison between the admins and the players; helping take attendance and collecting dues.</p>
+                    </div>
+                </div>
+                <div class="panel panel-default">
+                    <div class="panel-heading">What are the benefits of being a team captain?</div>
+                    <div class="panel-body">
+                        <p>You get help split up the teams. You will have influence over what the team will be working on that current cycle. And you get a 25% discount on the respective cycle fee.</p>
+                    </div>
+                </div>
+                <div class="panel panel-default">
+                    <div class="panel-heading">How do I sign up as a team captain?</div>
+                    <div class="panel-body">
+                        <p>Captains must be able to play at least 3 of the 4 weeks of the cycle. You can indicate that you are interested in being a captain when signing up for the cycle. We will notify you if we need you to captain before the cycle sign up closes.</p>
                     </div>
                 </div>
                 <div class="panel panel-default">
                     <div class="panel-heading">What does FOCUS stand for?</div>
                     <div class="panel-body">
                         <p><strong>F</strong>ostering <strong>O</strong>rganized <strong>C</strong>ompetitive <strong>U</strong>ltimate <strong>S</strong>eries</p>
+                    </div>
+                </div>
+                <div class="panel panel-default">
+                    <div class="panel-heading">What is Ultimate?</div>
+                    <div class="panel-body">
+                        <blockquote style="font-size:1em;">
+                            <p>Ultimate, originally known as ultimate frisbee, is a non-contact team field sport played with a flying disc. Points are scored by passing the disc to a teammate in the opposing end zone. Other basic rules are that players must not take steps while holding the disc, and interceptions, incomplete passes, and passes out of bounds are turnovers. Rain, wind, or occasionally other adversities can make for a testing match with rapid turnovers, heightening the pressure of play.</p>
+                            <footer><a href="https://en.wikipedia.org/wiki/Ultimate_(sport)">Wikipedia <cite title="Ultimate (sport)">Ultimate (sport)<cite></a></footer>
+                        </blockquote>
                     </div>
                 </div>
             </div>
