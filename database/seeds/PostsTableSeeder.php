7c9f7c3de310395196bc2e2983f22dfba7dd3abd
Asif Mohammed <asifm42@gmail.com>
2016-03-05 01:14:44 -0600

    updating first post

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -14,7 +14,7 @@ class PostsTableSeeder extends Seeder
         factory(App\Models\Post::class)->create([
             'type' => 'news',
             'title' => 'FOCUS League Officially Announced',
-            'content' => 'I am proud and excited to officially announce the FOCUS League. User accounts and Cycle 1 Sign ups are scheduled to open on March 9th, 2016. Make sure to check the FAQ page for more details.',
+            'content' => 'I am proud and excited to officially announce the FOCUS League. User accounts and Cycle 1 Sign ups are scheduled to open on March 9th, 2016. First game will be on Tuesday, March 15th, 2016 at 8p. Make sure to check the FAQ page for more details.',
             'posted_by' => 1,
             'created_at' => '2016-03-05 09:00:00',
             'updated_at'=> '2016-03-05 09:00:00',
