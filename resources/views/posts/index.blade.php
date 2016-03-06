ed2ec4281795908a1c4c22e67e4c2d42599a49f8
Asif Mohammed <asifm42@gmail.com>
2016-03-05 18:25:22 -0600

    making the post cards full width

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -9,9 +9,9 @@
             <p>News updates for the FOCUS League community</p>
         </div>
     </div>
-    <div class="container-fluid">
+    <div class="container">
         <div class="row">
-            <div class="col-xs-12 col-md-10 col-md-offset-1">
+            <div class="col-xs-12 ">
                 @foreach($posts as $post)
                 <div class="panel panel-default">
                     <div class="panel-heading">
