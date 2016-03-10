1a22fba4c9646eadc5c746757e0d0caff608d9ce
Asif Mohammed <asifm42@gmail.com>
2016-03-10 09:47:46 -0600

    adding 'if'

--- resources/views/emails/sub_signup.blade.php
+++ resources/views/emails/sub_signup.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <p>Thanks for signing up as a sub for Cycle {{ $cycle['name'] }} - Week {{ $week_index }} on {{ $date }}!</p> As mentioned before, a sub spot is not guaranteed. We'll be in touch soon if we need your talent.
 
-    <p>Your sub fee is $10. We'll charge your account when we place you on a team.</p>
+    <p>Your sub fee is $10. We'll charge your account if and when we place you on a team.</p>
 
     <p>See who else is signed up and other <a href="{{ route('cycles.view', $cycle['id']) }}">cycle details here</a>.</p>
 
