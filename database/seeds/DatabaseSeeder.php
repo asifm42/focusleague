c4f1f6a9a6203a5ec953e57d499fa84db7006f1d
Asif Mohammed <asifm42@gmail.com>
2016-03-13 16:20:31 -0500

    updating seeding for development

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,19 +12,20 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(AsifAdminAccountSeeder::class);
-        $this->call(FirstCycleSeeder::class);
-        $this->call(FirstCycleWeeksSeeder::class);
-        $this->call(AnnouncementPostSeeder::class);
-        $this->call(AsifFirstCycleSignupSeeder::class);
         $this->call(AsifUltimateHistorySignupSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(PostsTableSeeder::class);
+        $this->call(CyclesTableSeeder::class);
+        $this->call(WeeksTableSeeder::class);
+        $this->call(CycleUserTableSeeder::class);
+        $this->call(AvailabilityTableSeeder::class);
+        $this->call(SubsTableSeeder::class);
+        //
+        // $this->call(FirstCycleSeeder::class);
+        // $this->call(FirstCycleWeeksSeeder::class);
+        // $this->call(AnnouncementPostSeeder::class);
+        // $this->call(AsifFirstCycleSignupSeeder::class);
 
-        // $this->call(UsersTableSeeder::class);
-        // $this->call(PostsTableSeeder::class);
-        // $this->call(CyclesTableSeeder::class);
-        // $this->call(WeeksTableSeeder::class);
         // $this->call(GamesTableSeeder::class);
-        // $this->call(CycleUserTableSeeder::class);
-        // $this->call(AvailabilityTableSeeder::class);
-        // $this->call(SubsTableSeeder::class);
     }
 }
