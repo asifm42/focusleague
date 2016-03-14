77c0ea4cb0ace61a0c8109a6766055fb92322ad7
Asif Mohammed <asifm42@gmail.com>
2016-03-14 16:48:44 -0500

    user show method

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -96,22 +96,47 @@ class UsersController extends Controller
             $data['next_cycle_signup'] = [];
         }
 
-        // $data['invoices'] = $user->invoices();
-        // $data['upcomingInvoice'] = $user->upcomingInvoice();
+        return view('users.dashboard', $data );
+    }
 
-        // set up the navigation options
-        // $data['navigation_select'] = [
-        //     '#profile'          => 'Profile',
-        //     '#storage'          => 'Storage'
-        // ];
+    /**
+     * Show the user's dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Request $request, $id)
+    {
+        $user = User::findOrFail($id);
+        $user->load('ultimateHistory');
+        // dd($user->availability()->where('cycle_id',2)->get());
+        $data['user'] = $user;
+        $data['current_cycle'] = Cycle::current_cycle();
+        $data['next_cycle'] = Cycle::next_cycle();
+        $data['current_cycle_sub_weeks'] = [];
+        $data['next_cycle_sub_weeks'] = [];
+        if ($data['current_cycle']) {
+            $data['current_cycle_signup'] = $user->current_cycle_signup();
 
-        // add tags if needed
-        // if ($user->onFreeTrial()
-        //     || $user->onPaidPlan()
-        //     || $user->isAdmin()
-        //     || $user->isDev()) {
-        //     $data['navigation_select']['#tags'] = 'Tags';
-        // }
+            foreach($data['current_cycle']->weeks as $week){
+                $sub_deets = $week->subs->find($user->id);
+                if ($sub_deets){
+                    $data['current_cycle_sub_weeks'][] = ['week'=>$week,'deets'=>$sub_deets];
+                }
+            }
+        } else {
+            $data['current_cycle_signup'] = [];
+        }
+        if ($data['next_cycle']) {
+            $data['next_cycle_signup'] = $user->cycles()->where('cycle_id', $data['next_cycle']->id)->first();
+            foreach($data['next_cycle']->weeks as $week){
+                $sub_deets = $week->subs->find($user->id);
+                if ($sub_deets){
+                    $data['next_cycle_sub_weeks'][] = ['week'=>$week,'deets'=>$sub_deets];
+                }
+            }
+        } else {
+            $data['next_cycle_signup'] = [];
+        }
 
         return view('users.dashboard', $data );
     }
