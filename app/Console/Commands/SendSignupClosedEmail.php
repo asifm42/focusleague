67160ea52eb38720828a93c8e04dbad85a3fbf3f
Asif Mohammed <asifm42@gmail.com>
2016-03-14 21:56:21 -0500

    initial support for signup closed email

---
+++ app/Console/Commands/SendSignupClosedEmail.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\Cycle;
+use App\Mailers\UserMailer;
+
+class SendSignupClosedEmail extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'emails:sendSignupClosedEmail';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $cycle = Cycle::current_cycle();
+        $mailer = new UserMailer;
+
+        $cycle->signups()->each(function($item, $key) use ($mailer, $cycle) {
+            $mailer->sendSignupClosedEmail($item, $cycle);
+        });
+    }
+}
---
+++ resources/views/emails/signup_closed.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.email')
+
+@section('content')
+
+<p>
+Thank you for signing up for the first FOCUS League cycle; Cycle 2016-01.
+</p>
+
+<p>
+Demand was much more than anticipated with 61 sign-ups! We are honored by the support from the community.
+</p>
+
+<p>
+Sign-up is now closed and we are working on selecting captains and setting up teams. If you know of any one that wants to sign up or if you need to edit your availability, then please reply back to this email and we will try to accommodate to the best of our ability.
+</p>
+
+<p>
+Teams, format &amp; schedule will be announced by 6p tmw.
+</p>
+
+<p>
+We currently have field 3 at 8p. Games will start around 8:15-8:20p allowing some time to warm-up. Please bring a white and a non-grey dark jersey.
+</p>
+
+<p>
+<a href="https://www.google.com/maps/place/Houston+Sports+Park/@29.6379651,-95.3959319,15z/data=!4m2!3m1!1s0x0:0xfb9729c16219059c">Map link Houston Sports Park</a>
+</p>
+
+<p>
+See you at HSP tomorrow!
+</p>
+
+
+@endsection
