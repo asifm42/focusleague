b1ab0e48f743bb0e84377e09dc9da2d97838babd
Asif Mohammed <asifm42@gmail.com>
2016-03-15 16:01:09 -0500

    league cost config

---
+++ config/focus_cost.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | FOCUS League costs
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the costs that the focus player may endure.
+    |
+    |
+    */
+
+    'cycle' => [
+        'sub' => 10,
+        'two_weeks' => 18,
+        'three_weeks' => 21,
+        'four_weeks' => 24,
+    ],
+
+];
