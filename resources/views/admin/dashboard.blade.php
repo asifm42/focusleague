802aa19ac363f643b8a56655ad705d17c5556a4f
Asif Mohammed <asifm42@gmail.com>
2016-03-22 13:10:06 -0500

    updated Admin capabilities

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -21,14 +21,39 @@
                             <dd>{{ $current_cycle->name }}</dd>
                             <dt>Format</dt>
                             <dd>{{ $current_cycle->format }}</dd>
+                            <dt>Teams published?</dt>
+                            <dd>{{ ($current_cycle->teams_publised) ? 'Yes' : 'No' }}</dd>
+                            <dt>Status</dt>
+                            <dd>{{ $current_cycle->status() }}</dd>
                         </dl>
+                        <a href="{{ route('admin.cycle.details', $current_cycle->id) }}" class="btn btn-default btn-lg btn-block">Admin overview</a>
+                        @if (!$current_cycle->teams_publised)
+                            <a href="{{ route('cycle.teams.create', $current_cycle->id) }}" class="btn btn-default btn-lg btn-block">Team Builder</a>
+                            <a href="{{-- {{ route('games.create', $current_cycle->id) }} --}}" class="btn btn-default btn-lg btn-block">Schedule Builder</a>
+                            <a href="{{ route('cycle.teams.publish', $current_cycle->id) }}" class="btn btn-default btn-lg btn-block">Publish teams</a>
+                        @else
+                            <a href="{{ route('cycle.teams.unpublish', $current_cycle->id) }}" class="btn btn-default btn-lg btn-block">Place a sub</a>
+                            <a href="{{ route('cycle.teams.unpublish', $current_cycle->id) }}" class="btn btn-default btn-lg btn-block">Unpublish teams</a>
+                        @endif
                     </div>
                 </div>
             @endif
             </div>
+            <div class="col-xs-12 col-md-6">
+
+                <div class="panel panel-default">
+                    <div class="panel-heading">Admin Tasks</div>
+                    <div class="panel-body">
+                        <a href="{{ route('users.list') }}" class="btn btn-default btn-lg btn-block">See all users</a>
+                        <a href="{{ route('posts.create') }}" class="btn btn-default btn-lg btn-block">Create a post</a>
+                        <a href="{{ route('transactions.create') }}" class="btn btn-default btn-lg btn-block">Post a transaction</a>
+                    </div>
+                </div>
+
+            </div>
         </div>
 
-
+{{--
 
         <div class="row">
             <div class="col-xs-12 col-md-4">
@@ -65,5 +90,6 @@
             @include('signups.panel', $data = ['signups'=>$mixedOnlyFemale, 'cycle'=>$current_cycle, 'title' => 'Mixed only - female'])
             </div>
         </div>
+        --}}
     </div>
 @endsection
