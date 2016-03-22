ce5d2c2309a28d39f32902f852a3558d3593fdd7
Asif Mohammed <asifm42@gmail.com>
2016-03-22 12:33:18 -0500

    adding Sub model

---
+++ app/Models/Sub.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\TrimScalarValues;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Sub extends Model
+{
+    use TrimScalarValues, SoftDeletes;
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = ['created_at', 'updated_at', 'deleted_at'];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'week_id', 'user_id', 'note', 'team_id'
+    ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [
+
+    ];
+
+    /**
+     * Get the sub's week
+     */
+    public function week()
+    {
+        return $this->belongsTo('App\Models\Week');
+    }
+
+    /**
+     * Get the Sub's user info
+     */
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User');
+    }
+
+    /**
+     * Get the Sub's team
+     */
+    public function team()
+    {
+        return $this->belongsTo('App\Models\Team');
+    }
+}
