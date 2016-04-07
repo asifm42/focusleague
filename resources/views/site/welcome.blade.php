f96d016a71dc33013f22293f1d73a5caa140219b
Asif Mohammed <asifm42@gmail.com>
2016-04-07 16:54:09 -0500

    updated welcome page, cycle sign up link, dashboard button if signed in

--- resources/views/site/welcome.blade.php
+++ resources/views/site/welcome.blade.php
@@ -16,8 +16,16 @@
                         <h4>Game Status for Tuesday, Mar 29</h4>
                         <p>We are aware of the potential rainout. As of 4:45pm, Games are on schedule. If HSP is closed, games will be canceled. HSP will communicate park closure via their <a href="http://www.houstonsportspark.com">website</a> and <a href="https://twitter.com/HoustonSportsPk?ref_src=twsrc%5Etfw">twitter feed</a>.</p>
                     </div>
-                    <h6>Every Tuesday, 8p-10p, starting March 15 at the Houston Sports Park</h6>
-                    <h4 class="text-info">Sign up for Cycle 2016-02 is now open!</h4>
+                    <h6>Tuesdays, 8p-10p, at the <a href="https://www.google.com/maps/place/Houston+Sports+Park/@29.6379651,-95.3959319,15z/data=!4m2!3m1!1s0x0:0xfb9729c16219059c">Houston Sports Park</a></h6>
+
+                    <h4><a href="{{ route('cycles.view', 'current') }}">Sign up for Cycle 2016-02 is now open!</a></h4>
+
+                    @if(auth()->check())
+                        <a href="{{ route('users.dashboard') }}" class ="btn btn-primary btn-lg" style="margin-bottom:15px;"><i class="fa fa-tachometer"></i>&nbsp; Dashboard</a>
+                    @else
+                        <a href="{{ route('users.create') }}" class ="btn btn-primary btn-lg" style="margin-bottom:15px;">Get your player account here</a>
+                    @endif
+
                     <div class="jumbotron">
                         <h4>The Mission</h4>
                         <p>To structure would-be pickup games into a league and increase the availability of competitive Ultimate in Houston.</p>
@@ -30,9 +38,16 @@
                         <h3>Game Status for Tuesday, Mar 29</h3>
                         <p>We are aware of the potential rainout. As of 4:45pm, Games are on schedule. If HSP is closed, games will be canceled. HSP will communicate park closure via their <a href="http://www.houstonsportspark.com">website</a> and <a href="https://twitter.com/HoustonSportsPk?ref_src=twsrc%5Etfw">twitter feed</a>.</p>
                     </div>
-                    <h5>Every Tuesday, 8p-10p, starting March 15 at the <a href="https://www.google.com/maps/place/Houston+Sports+Park/@29.6379651,-95.3959319,15z/data=!4m2!3m1!1s0x0:0xfb9729c16219059c">Houston Sports Park</a></h5>
-                    <h4 class="text-info">Sign up for Cycle 2016-02 is now open!</h4>
-                    <a href="{{ route('users.create') }}" class ="btn btn-primary btn-lg" style="margin-bottom:15px;">Get your player account here</a>
+                    <h5>Tuesdays, 8p-10p, at the <a href="https://www.google.com/maps/place/Houston+Sports+Park/@29.6379651,-95.3959319,15z/data=!4m2!3m1!1s0x0:0xfb9729c16219059c">Houston Sports Park</a></h5>
+
+                    <h4><a href="{{ route('cycles.view', 'current') }}">Sign up for Cycle 2016-02 is now open!</a></h4>
+
+                    @if(auth()->check())
+                        <a href="{{ route('users.dashboard') }}" class ="btn btn-primary btn-lg" style="margin-bottom:15px;"><i class="fa fa-tachometer"></i>&nbsp; Dashboard</a>
+                    @else
+                        <a href="{{ route('users.create') }}" class ="btn btn-primary btn-lg" style="margin-bottom:15px;">Get your player account here</a>
+                    @endif
+
                     <div class="jumbotron">
                         <h4>The Mission</h4>
                         <p>To structure would-be pickup games into a league and increase the availability of competitive Ultimate in Houston.</p>
