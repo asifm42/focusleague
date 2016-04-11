9114d2b7ce420a57045d5afeffa1eda9c773dd03
Asif Mohammed <asifm42@gmail.com>
2016-04-11 14:32:25 -0500

    accessing arrays correctly

--- resources/views/emails/non_returner_reminder.blade.php
+++ resources/views/emails/non_returner_reminder.blade.php
@@ -2,9 +2,9 @@
 
 @section('content')
 
-    <p>We noticed that you played in the last cycle but haven't signed up for Cycle {{ $cycle->name }} yet. We just wanted to check in with you to make sure you enjoyed your experience last cycle. If you have any feedback on the league, we would love to hear it. Please feel free to share your thoughts with us by replying to this email.</p>
+    <p>We noticed that you played in the last cycle but haven't signed up for Cycle {{ $cycleArr['name'] }} yet. We just wanted to check in with you to make sure you enjoyed your experience last cycle. If you have any feedback on the league, we would love to hear it. Please feel free to share your thoughts with us by replying to this email.</p>
 
-    <p>If signing-up simply slipped your mind, no sweat! It's not too late to <a href="{{ route('cycle.signup.create', $cycle->id ) }}">SIGN UP FOR CYCLE {{ $cycle->name }}</a>.</p>
+    <p>If signing-up simply slipped your mind, no sweat! It's not too late to <a href="{{ route('cycle.signup.create', $cycleArr['id'] ) }}">SIGN UP FOR CYCLE {{ $cycleArr['name'] }}</a>.</p>
 
     <p>Thanks for your support!</p>
 @stop
--- resources/views/emails/signup_closing_reminder.blade.php
+++ resources/views/emails/signup_closing_reminder.blade.php
@@ -2,9 +2,9 @@
 
 @section('content')
 
-    <p>Just a quick friendly reminder that sign-up for Cycle {{ $cycle->name }} is closing soon!{{-- at {{ $cycle->signup_closes_at->toDayDateTimeString() }}.--}} We would hate to not see your beautiful face out there.</p>
+    <p>Just a quick friendly reminder that sign-up for Cycle {{ $cycleArr['name'] }} is closing soon!{{-- at {{ $cycle->signup_closes_at->toDayDateTimeString() }}.--}} We would hate to not see your beautiful face out there.</p>
 
-    <p><a href="{{ route('cycle.signup.create', $cycle->id) }}">SIGN UP FOR CYCLE {{ $cycle->name }}</a></p>
+    <p><a href="{{ route('cycle.signup.create', $cycleArr['id']) }}">SIGN UP FOR CYCLE {{ $cycleArr['name'] }}</a></p>
 
     <p>Thanks for your support!</p>
 @stop
