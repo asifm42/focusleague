dc77beccd4e061c2cb4a33ccea5b4b94eeda5f48
Asif Mohammed <asifm42@gmail.com>
2016-04-26 10:11:38 -0500

    updated message for Apr 26 game status. Added alt to logo image

--- resources/views/site/welcome.blade.php
+++ resources/views/site/welcome.blade.php
@@ -9,18 +9,18 @@
         <div class="row">
             <div class="col-xs-12">
                 <div class="text-center hidden-md hidden-lg">
-                    <div><img src="{{ asset('assets/img/logo.png') }}" class="logo-welcome-mobile"></div>
+                    <div><img alt="FOCUS League" title="FOCUS League" src="{{ asset('assets/img/logo.png') }}" class="logo-welcome-mobile"></div>
                     <h5>Fostering Organized Competitive Ultimate Series</h5>
                     <!-- <h5><span class = "emphasize">F</span>ostering <span class = "emphasize">O</span>rganized <strong>C</strong>ompetitive <strong>U</strong>ltimate <strong>S</strong>eries</h5> -->
                     <div class="jumbotron" style="background-color: #ffff99;" >
-                        <h4>Game Status for Tuesday, Apr 19</h4>
-                        <p>Games are canceled due to weather. See you next week!</p>
-                        <p class="hidden">We are aware of the potential rainout. If HSP is closed, games will be canceled. HSP will communicate park closure via their <a href="http://www.houstonsportspark.com">website</a> and <a href="https://twitter.com/HoustonSportsPk?ref_src=twsrc%5Etfw">twitter feed</a>.</p>
+                        <h4>Game Status for Tuesday, Apr 26</h4>
+                        <p class="hidden">Games are canceled due to weather. See you next week!</p>
+                        <p>As of 10 am, Games are on. However, if HSP is closed due to excessive rain in the last week, games will be canceled. HSP will communicate park closure via their <a href="http://www.houstonsportspark.com">website</a> and <a href="https://twitter.com/HoustonSportsPk?ref_src=twsrc%5Etfw">twitter feed</a>.</p>
                     </div>
                     <h6>Tuesdays, 8p-10p, at the <a href="https://www.google.com/maps/place/Houston+Sports+Park/@29.6379651,-95.3959319,15z/data=!4m2!3m1!1s0x0:0xfb9729c16219059c">Houston Sports Park</a></h6>
 
                     @if(auth()->check())
-                        <p><a href="{{ route('users.dashboard') }}" class ="btn btn-primary btn-lg" ><i class="fa fa-tachometer"></i>&nbsp; Dashboard</a></p>
+                        <p><a href="{{ route('users.dashboard') }}" class ="btn btn-primary btn-lg" ><i class="fa fa-tachometer"></i>&nbsp; Your Dashboard</a></p>
                         @if($current_cycle && !auth()->user()->current_cycle_signup())
                             @if($current_cycle->isSignupOpen())
                                 <p><a href="{{ route('cycle.signup.create', 'current') }}" class ="btn btn-primary btn-lg">Sign up for Cycle 2016-02</a></p>
@@ -45,14 +45,14 @@
                     <div><img src="{{ asset('assets/img/logo.png') }}" class="logo-welcome-desktop"></div>
                     <h3>Fostering Organized Competitive Ultimate Series</h3>
                     <div class="jumbotron" style="background-color: #ffff99;">
-                        <h3>Game Status for Tuesday, Apr 19</h3>
-                        <p>Games are canceled due to weather. See you next week!</p>
-                        <p class="hidden">We are aware of the potential rainout. If HSP is closed, games will be canceled. HSP will communicate park closure via their <a href="http://www.houstonsportspark.com">website</a> and <a href="https://twitter.com/HoustonSportsPk?ref_src=twsrc%5Etfw">twitter feed</a>.</p>
+                        <h3>Game Status for Tuesday, Apr 26</h3>
+                        <p class="hidden">Games are canceled due to weather. See you next week!</p>
+                        <p>As of 10 am, Games are on. However, if HSP is closed due to excessive rain in the last week, games will be canceled. HSP will communicate park closure via their <a href="http://www.houstonsportspark.com">website</a> and <a href="https://twitter.com/HoustonSportsPk?ref_src=twsrc%5Etfw">twitter feed</a>.</p>
                     </div>
                     <h5>Tuesdays, 8p-10p, at the <a href="https://www.google.com/maps/place/Houston+Sports+Park/@29.6379651,-95.3959319,15z/data=!4m2!3m1!1s0x0:0xfb9729c16219059c">Houston Sports Park</a></h5>
 
                     @if(auth()->check())
-                        <p><a href="{{ route('users.dashboard') }}" class ="btn btn-primary btn-lg" ><i class="fa fa-tachometer"></i>&nbsp; Dashboard</a></p>
+                        <p><a href="{{ route('users.dashboard') }}" class ="btn btn-primary btn-lg" ><i class="fa fa-tachometer"></i>&nbsp; Your Dashboard</a></p>
                         @if($current_cycle && !auth()->user()->current_cycle_signup())
                             @if($current_cycle->isSignupOpen())
                                 <p><a href="{{ route('cycle.signup.create', 'current') }}" class ="btn btn-primary btn-lg">Sign up for Cycle 2016-02</a></p>
