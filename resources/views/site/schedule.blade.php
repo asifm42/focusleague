f783fec9020cc68b36bc8a3c55ca0e8e81e87f10
Asif Mohammed <asifm42@gmail.com>
2016-05-23 17:42:25 -0500

    adding schedule to welcome page

---
+++ resources/views/site/schedule.blade.php
@@ -0,0 +1,49 @@
+                <div class="row">
+                    <div class="col-xs-12 col-md-6 col-md-offset-3">
+                        <div class="list-group text-left">
+                          <li class="list-group-item list-group-item-success text-center">
+                            <h5 class="list-group-item-heading">Schedule</h5>
+                            <h6 class="list-group-item-heading">Tuesdays, 8p-10p, at the <a href="https://www.google.com/maps/place/Houston+Sports+Park/@29.6379651,-95.3959319,15z/data=!4m2!3m1!1s0x0:0xfb9729c16219059c">Houston Sports Park</a></h6>
+                          </li>
+                          <li class="list-group-item">
+                            Tuesday, May 24th - NO game
+                          </li>
+                          <li class="list-group-item list-group-item-info">
+                            Wednesday, May 25th - Cycle 2016-04 Registration Opens
+                          </li>
+                          <li class="list-group-item">
+                            Tuesday, May 31st - NO game
+                          </li>
+                          <li class="list-group-item">
+                            Tuesday, June 7th - Cycle 2016-04 Wk1
+                          </li>
+                          <li class="list-group-item">
+                            Tuesday, June 14th - Cycle 2016-04 Wk2
+                          </li>
+                          <li class="list-group-item">
+                            Tuesday, June 21st - Cycle 2016-04 Wk3
+                          </li>
+                          <li class="list-group-item">
+                            Tuesday, June 28th - Cycle 2016-04 Wk4
+                          </li>
+                          <li class="list-group-item">
+                            July - No games
+                          </li>
+                          <li class="list-group-item list-group-item-info">
+                            Wednesday, July 20th - Cycle 2016-05 Registration Opens
+                          </li>
+                          <li class="list-group-item">
+                            Tuesday, August 2nd - Cycle 2016-05 Wk1
+                          </li>
+                          <li class="list-group-item">
+                            Tuesday, August 9th - Cycle 2016-05 Wk2
+                          </li>
+                          <li class="list-group-item">
+                            Tuesday, August 16th - Cycle 2016-05 Wk3
+                          </li>
+                          <li class="list-group-item">
+                            Tuesday, August 23rd - Cycle 2016-05 Wk4
+                          </li>
+                        </div>
+                    </div>
+                </div>
--- resources/views/site/welcome.blade.php
+++ resources/views/site/welcome.blade.php
@@ -9,95 +9,200 @@
         <div class="row">
             <div class="col-xs-12">
                 <div class="text-center hidden-md hidden-lg">
-                    <div><img alt="FOCUS League" title="FOCUS League" src="{{ asset('assets/img/logo.png') }}" class="logo-welcome-mobile"></div>
-                    <h5>Fostering Organized Competitive Ultimate Series</h5>
+                    <div>
+                        <img alt="FOCUS League" title="FOCUS League" src="{{ asset('assets/img/logo.png') }}" class="logo-welcome-mobile">
+                    </div>
+                    <h5>
+                        Fostering Organized Competitive Ultimate Series
+                    </h5>
                     <!-- <h5><span class = "emphasize">F</span>ostering <span class = "emphasize">O</span>rganized <strong>C</strong>ompetitive <strong>U</strong>ltimate <strong>S</strong>eries</h5> -->
+
+                    <div class="text-center">
+                        <div class="jumbotron">
+                            <h4>
+                                The Mission
+                            </h4>
+                            <p>
+                                To structure would-be pickup games into a league and increase the availability of competitive Ultimate in Houston.
+                            </p>
+                            <p>
+                                <a  href="{{ route('site.faq') }}"
+                                    class="btn btn-default btn-lg">
+                                        Learn more
+                                </a>
+                            </p>
+                        </div>
+                    </div>
+
                     @if($current_cycle && $current_cycle->gameToday())
-                        <div class="jumbotron" style="background-color: #ffff99;" >
-                            <h4>Game Status for {{ Carbon::today()->format("l, F jS") }}</h4>
+                        <div    class="jumbotron"
+                                style="background-color: #ffff99;" >
+                            <h4>
+                                Game Status for {{ Carbon::today()->format("l, F jS") }}
+                            </h4>
                             @if($current_cycle->currentWeek()->isRainedOut())
                                 <p>Games are canceled due to weather.</p>
                             @else
-                                <p><strong>Games are ON!</strong> We are on the turf field, so the game will be ON even if the grass fields are closed. We have a very low probability of cancelling due to lightning.</p>
-                                <p>HSP will communicate lightning closure/delay via their <a href="https://twitter.com/HoustonSportsPk?ref_src=twsrc%5Etfw">twitter feed</a>. Please check that or here before leaving for the fields.</p>
+                                <p>
+                                    <strong>Games are ON!</strong> We are on the turf field, so the game will be ON even if the grass fields are closed. We have a very low probability of cancelling due to lightning.
+                                </p>
+                                <p>
+                                    HSP will communicate lightning closure/delay via their <a href="https://twitter.com/HoustonSportsPk?ref_src=twsrc%5Etfw">twitter feed</a>. Please check that or here before leaving for the fields.
+                                </p>
                             {{--
                                 <p>Games are on. However, if HSP is closed due to weather, games will be canceled. HSP will communicate park closure via their <a href="http://www.houstonsportspark.com">website</a> and <a href="https://twitter.com/HoustonSportsPk?ref_src=twsrc%5Etfw">twitter feed</a>. Check one of those resources before heading to the fields.</p>
                             --}}
                             @endif
                         </div>
                     @endif
-                    <h6>Tuesdays, 8p-10p, at the <a href="https://www.google.com/maps/place/Houston+Sports+Park/@29.6379651,-95.3959319,15z/data=!4m2!3m1!1s0x0:0xfb9729c16219059c">Houston Sports Park</a></h6>
 
                     @if(auth()->check())
-                        <p><a href="{{ route('users.dashboard') }}" class ="btn btn-primary btn-lg" ><i class="fa fa-tachometer"></i>&nbsp; Your Dashboard</a></p>
+                        <p>
+                            <a  href="{{ route('users.dashboard') }}"
+                                class ="btn btn-primary btn-lg" >
+                                    <i class="fa fa-tachometer"></i>&nbsp; Your Dashboard
+                            </a>
+                        </p>
                         @if($current_cycle && !auth()->user()->current_cycle_signup())
                             @if($current_cycle->isSignupOpen())
-                                <p><a href="{{ route('cycle.signup.create', 'current') }}" class ="btn btn-primary btn-lg">Sign up for Cycle {{ $current_cycle->name }}</a></p>
+                                <p>
+                                    <a  href="{{ route('cycle.signup.create', 'current') }}"
+                                        class ="btn btn-primary btn-lg">
+                                            Sign up for Cycle {{ $current_cycle->name }}
+                                    </a>
+                                </p>
                             @else
-                                <h5 class="text-info">Sign up for Cycle {{ $current_cycle->name }} closed at {{ $current_cycle->signup_closes_at->format('M j g:i a') }} but you can still sign up as a sub.</h5>
+                                <h5 class="text-info">
+                                    Sign up for Cycle {{ $current_cycle->name }} closed at {{ $current_cycle->signup_closes_at->format('M j g:i a') }} but you can still sign up as a sub.
+                                </h5>
                             @endif
-                            <p><a href="{{ route('sub.create', 'current') }}" class ="btn btn-info btn-lg">Sign up as a sub for Cycle {{ $current_cycle->name }}</a></p>
+                            <p>
+                                <a  href="{{ route('sub.create', 'current') }}"
+                                    class ="btn btn-info btn-lg">
+                                        Sign up as a sub for Cycle {{ $current_cycle->name }}
+                                </a>
+                            </p>
                         @endif
                     @else
                         @if($current_cycle && $current_cycle->isSignupOpen())
-                            <h5 class="text-primary">Sign up for Cycle {{ $current_cycle->name }} is now open!</h5>
+                            <h5 class="text-primary">
+                                Sign up for Cycle {{ $current_cycle->name }} is now open!
+                            </h5>
                         @elseif ($current_cycle)
-                            <h5 class="text-info">Sign up for Cycle {{ $current_cycle->name }} closed at {{ $current_cycle->signup_closes_at->format('M j g:i a') }} but you can still sign up as a sub.</h5>
+                            <h5 class="text-info">
+                                Sign up for Cycle {{ $current_cycle->name }} closed at {{ $current_cycle->signup_closes_at->format('M j g:i a') }} but you can still sign up as a sub.
+                            </h5>
                         @endif
-                        <p><a href="{{ route('sessions.create', 2) }}" class="btn btn-primary btn-lg"><i class="fa fa-sign-in"></i>&nbsp; Sign in</a></p>
+                        <p>
+                            <a  href="{{ route('sessions.create', 2) }}"
+                                class="btn btn-primary btn-lg"><i class="fa fa-sign-in"></i>&nbsp; Sign in</a>
+                        </p>
                     @endif
+
+                    @include('site.schedule')
+
                     @if($next_cycle)
-                        <h5 class="text-info">Registration for Cycle {{ $next_cycle->name }} opens on {{ $next_cycle->signup_opens_at->format('M j') }}!</h5>
+                        <h5 class="text-info">
+                            Registration for Cycle {{ $next_cycle->name }} opens on {{ $next_cycle->signup_opens_at->format('M j') }}!
+                        </h5>
                     @endif
                 </div>
                 <div class="text-center hidden-xs hidden-sm">
-                    <div><img src="{{ asset('assets/img/logo.png') }}" class="logo-welcome-desktop"></div>
-                    <h3>Fostering Organized Competitive Ultimate Series</h3>
+                    <div>
+                        <img src="{{ asset('assets/img/logo.png') }}" class="logo-welcome-desktop">
+                    </div>
+                    <h3>
+                        Fostering Organized Competitive Ultimate Series
+                    </h3>
+
+                    <div class="text-center">
+                        <div class="jumbotron">
+                            <h4>
+                                The Mission
+                            </h4>
+                            <p>
+                                To structure would-be pickup games into a league and increase the availability of competitive Ultimate in Houston.
+                            </p>
+                            <p>
+                                <a  href="{{ route('site.faq') }}"
+                                    class="btn btn-default btn-lg">
+                                        Learn more
+                                </a>
+                            </p>
+                        </div>
+                    </div>
                     @if($current_cycle && $current_cycle->gameToday())
                     <div class="jumbotron" style="background-color: #ffff99;" >
-                        <h3>Game Status for {{ Carbon::today()->format("l, F jS") }}</h3>
+                            <h3>
+                                Game Status for {{ Carbon::today()->format("l, F jS") }}
+                            </h3>
                         @if($current_cycle->currentWeek()->isRainedOut())
-                        <p>Games are canceled due to weather.</p>
+                            <p>
+                                Games are canceled due to weather.
+                            </p>
                         @else
-                                <p><strong>Games are ON!</strong> We are on the turf field, so the game will be ON even if the grass fields are closed. We have a very low probability of cancelling due to lightning.</p>
-                                <p>HSP will communicate lightning closure/delay via their <a href="https://twitter.com/HoustonSportsPk?ref_src=twsrc%5Etfw">twitter feed</a>. Please check that or here before leaving for the fields.</p>
+                                <p>
+                                    <strong>Games are ON!</strong> We are on the turf field, so the game will be ON even if the grass fields are closed. We have a very low probability of cancelling due to lightning.
+                                </p>
+                                <p>
+                                    HSP will communicate lightning closure/delay via their <a href="https://twitter.com/HoustonSportsPk?ref_src=twsrc%5Etfw">twitter feed</a>. Please check that or here before leaving for the fields.
+                                </p>
                             {{--
                                 <p>Games are on. However, if HSP is closed due to weather, games will be canceled. HSP will communicate park closure via their <a href="http://www.houstonsportspark.com">website</a> and <a href="https://twitter.com/HoustonSportsPk?ref_src=twsrc%5Etfw">twitter feed</a>. Check one of those resources before heading to the fields.</p>
                             --}}
                         @endif
                     </div>
                     @endif
-                    <h5>Tuesdays, 8p-10p, at the <a href="https://www.google.com/maps/place/Houston+Sports+Park/@29.6379651,-95.3959319,15z/data=!4m2!3m1!1s0x0:0xfb9729c16219059c">Houston Sports Park</a></h5>
 
                     @if(auth()->check())
-                        <p><a href="{{ route('users.dashboard') }}" class ="btn btn-primary btn-lg" ><i class="fa fa-tachometer"></i>&nbsp; Your Dashboard</a></p>
+                        <p>
+                            <a  href="{{ route('users.dashboard') }}"
+                                class ="btn btn-primary btn-lg" >
+                                    <i class="fa fa-tachometer"></i>&nbsp; Your Dashboard
+                            </a>
+                        </p>
                         @if($current_cycle && !auth()->user()->current_cycle_signup())
                             @if($current_cycle->isSignupOpen())
-                                <p><a href="{{ route('cycle.signup.create', 'current') }}" class ="btn btn-primary btn-lg">Sign up for Cycle {{ $current_cycle->name }}</a></p>
+                                <p>
+                                    <a  href="{{ route('cycle.signup.create', 'current') }}"
+                                        class ="btn btn-primary btn-lg">
+                                        Sign up for Cycle {{ $current_cycle->name }}
+                                    </a>
+                                </p>
                             @else
                                 <h4 class="text-info">Sign up for Cycle {{ $current_cycle->name }} closed at {{ $current_cycle->signup_closes_at->format('M j g:i a') }} but you can still sign up as a sub.</h4>
                             @endif
-                            <p><a href="{{ route('sub.create', 'current') }}" class ="btn btn-info btn-lg">Sign up as a sub for Cycle {{ $current_cycle->name }}</a></p>
+                            <p>
+                                <a  href="{{ route('sub.create', 'current') }}"
+                                    class ="btn btn-info btn-lg">
+                                        Sign up as a sub for Cycle {{ $current_cycle->name }}
+                                </a>
+                            </p>
                         @endif
                     @else
                         @if($current_cycle && $current_cycle->isSignupOpen())
-                            <h4 class="text-primary">Sign up for Cycle {{ $current_cycle->name }} is now open!</h4>
+                            <h4 class="text-primary">
+                                Sign up for Cycle {{ $current_cycle->name }} is now open!
+                            </h4>
                         @elseif ($current_cycle)
-                            <h4 class="text-info">Sign up for Cycle {{ $current_cycle->name }} closed at {{ $current_cycle->signup_closes_at->format('M j g:i a') }} but you can still sign up as a sub.</h4>
+                            <h4 class="text-info">
+                                Sign up for Cycle {{ $current_cycle->name }} closed at {{ $current_cycle->signup_closes_at->format('M j g:i a') }} but you can still sign up as a sub.
+                            </h4>
                         @endif
-                        <p><a href="{{ route('sessions.create', 2) }}" class="btn btn-primary btn-lg"><i class="fa fa-sign-in"></i>&nbsp; Sign in</a></p>
+                        <p>
+                            <a  href="{{ route('sessions.create', 2) }}"
+                                class="btn btn-primary btn-lg">
+                                    <i class="fa fa-sign-in"></i>&nbsp; Sign in
+                            </a>
+                        </p>
                     @endif
                     @if($next_cycle)
-                        <h4 class="text-info">Registration for Cycle {{ $next_cycle->name }} opens on {{ $next_cycle->signup_opens_at->format('M j') }}!</h4>
+                        <h4 class="text-info">
+                            Registration for Cycle {{ $next_cycle->name }} opens on {{ $next_cycle->signup_opens_at->format('M j') }}!
+                        </h4>
                     @endif
-                </div>
 
-                <div class="text-center">
-                    <div class="jumbotron">
-                        <h4>The Mission</h4>
-                        <p>To structure would-be pickup games into a league and increase the availability of competitive Ultimate in Houston.</p>
-                        <p><a href="{{ route('site.faq') }}" class="btn btn-default btn-lg">Learn more</a></p>
-                    </div>
+                    @include('site.schedule')
                 </div>
             </div>
         </div>
