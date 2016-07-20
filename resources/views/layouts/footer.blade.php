7b9631d6892ca9621301b4d849f19e3cc38c20f2
Asif Mohammed <asifm42@gmail.com>
2016-07-19 21:10:41 -0500

    updated bylines

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,12 +3,12 @@
         <div class="page-footer text-muted">
             <div class="visible-xs-block">
                 {{-- &copy; {{ date("Y") }} <a href="http://focusleague.com">FOCUS League</a> --}}
-                <p><strong>FOCUS League</strong> &copy; {{ date("Y") }} is brought to you by Asif Mohammed with inspiration from Sean McCall's CSS 2002-2009<br />
+                <p><strong>FOCUS League</strong> &copy; {{ date("Y") }} is a competitive Ultimate <span style="text-decoration: line-through;">Frisbee</span> league in Houston, TX brought to you by Asif Mohammed with inspiration from Sean McCall's CSS 2002-2009<br />
                 <a href="http://focusleague.com">focusleague.com</a> designed &amp; developed by Asif Mohammed<br />
                 <strong>FOCUS League</strong> logo designed Nicky Carranza</p>
             </div>
             <div class="visible-sm-inline visible-md-inline visible-lg-inline">
-                <p><strong>FOCUS League</strong> &copy; {{ date("Y") }} is brought to you by Asif Mohammed with inspiration from Sean McCall's CSS 2002-2009<br />
+                <p><strong>FOCUS League</strong> &copy; {{ date("Y") }} is a competitive Ultimate <span style="text-decoration: line-through;">Frisbee</span> league in Houston, TX brought to you by Asif Mohammed with inspiration from Sean McCall's CSS 2002-2009<br />
                 <a href="http://focusleague.com">focusleague.com</a> designed &amp; developed by Asif Mohammed<br />
                 <strong>FOCUS League</strong> logo designed Nicky Carranza</p>
             </div>
