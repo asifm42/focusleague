825e1559e5f8c40acbeb55bf7c8649c5752ca9e0
Asif Mohammed <asifm42@gmail.com>
2017-02-13 11:07:31 -0600

    updated welcome page and schedule

--- resources/views/site/schedule.blade.php
+++ resources/views/site/schedule.blade.php
@@ -1,14 +1,14 @@
-<div class="row">
-    <div class="col-xs-12 col-md-6 col-md-offset-3">
-        <ul class="list-group text-left">
+
+        <ul class="list-group text-left schedule">
             <li class="list-group-item list-group-item-success text-center">
-                <h3 class="list-group-item-heading">
+                <h4 class="list-group-item-heading">
                     Schedule
-                </h3>
+                </h4>
                 <h6 class="list-group-item-heading">
                     Tuesdays, 8p-10p, at the <a href="https://www.google.com/maps/place/Houston+Sports+Park/@29.6379651,-95.3959319,15z/data=!4m2!3m1!1s0x0:0xfb9729c16219059c">Houston Sports Park</a>
                 </h6>
             </li>
+<div class="schedule-list">
             <li class="list-group-item list-group-item-info">
                 <time datetime="2017-03-01" class="icon">
                   <em>Wednesday</em>
@@ -24,8 +24,8 @@
                   <strong>March</strong>
                   <span>7</span>
                 </time>
-                <span class="event-xs hidden-md hidden-lg">Cycle 2017-01 Wk1</span>
-                <span class="event hidden-xs hidden-sm">Cycle 2017-01 Wk1</span>
+                <span class="event-xs hidden-md hidden-lg">Cycle 2017-01 Wk1 of 4</span>
+                <span class="event hidden-xs hidden-sm">Cycle 2017-01 Wk1 of 4</span>
             </li>
             <li class="list-group-item">
                 <time datetime="2017-03-07" class="icon">
@@ -33,8 +33,8 @@
                   <strong>March</strong>
                   <span>14</span>
                 </time>
-                <span class="event-xs hidden-md hidden-lg">Cycle 2017-01 Wk2</span>
-                <span class="event hidden-xs hidden-sm">Cycle 2017-01 Wk2</span>
+                <span class="event-xs hidden-md hidden-lg">Cycle 2017-01 Wk2 of 4</span>
+                <span class="event hidden-xs hidden-sm">Cycle 2017-01 Wk2 of 4</span>
             </li>
             <li class="list-group-item">
                 <time datetime="2017-03-07" class="icon">
@@ -42,8 +42,8 @@
                   <strong>March</strong>
                   <span>21</span>
                 </time>
-                <span class="event-xs hidden-md hidden-lg">Cycle 2017-01 Wk3</span>
-                <span class="event hidden-xs hidden-sm">Cycle 2017-01 Wk3</span>
+                <span class="event-xs hidden-md hidden-lg">Cycle 2017-01 Wk3 of 4</span>
+                <span class="event hidden-xs hidden-sm">Cycle 2017-01 Wk3 of 4</span>
             </li>
             <li class="list-group-item">
                 <time datetime="2017-03-07" class="icon">
@@ -51,8 +51,8 @@
                   <strong>March</strong>
                   <span>28</span>
                 </time>
-                <span class="event-xs hidden-md hidden-lg">Cycle 2017-01 Wk4</span>
-                <span class="event hidden-xs hidden-sm">Cycle 2017-01 Wk4</span>
+                <span class="event-xs hidden-md hidden-lg">Cycle 2017-01 Wk4 of 4</span>
+                <span class="event hidden-xs hidden-sm">Cycle 2017-01 Wk4 of 4</span>
             </li>
             <li class="list-group-item list-group-item-info">
                 <time datetime="2017-03-07" class="icon">
@@ -69,8 +69,8 @@
                   <strong>April</strong>
                   <span>4</span>
                 </time>
-                <span class="event-xs hidden-md hidden-lg">Cycle 2017-02 Wk1</span>
-                <span class="event hidden-xs hidden-sm">Cycle 2017-02 Wk1</span>
+                <span class="event-xs hidden-md hidden-lg">Cycle 2017-02 Wk1 of 4</span>
+                <span class="event hidden-xs hidden-sm">Cycle 2017-02 Wk1 of 4</span>
             </li>
             <li class="list-group-item">
                 <time datetime="2017-03-07" class="icon">
@@ -78,8 +78,8 @@
                   <strong>April</strong>
                   <span>11</span>
                 </time>
-                <span class="event-xs hidden-md hidden-lg">Cycle 2017-02 Wk2</span>
-                <span class="event hidden-xs hidden-sm">Cycle 2017-02 Wk2</span>
+                <span class="event-xs hidden-md hidden-lg">Cycle 2017-02 Wk2 of 4</span>
+                <span class="event hidden-xs hidden-sm">Cycle 2017-02 Wk2 of 4</span>
             </li>
             <li class="list-group-item">
                 <time datetime="2017-03-07" class="icon">
@@ -87,8 +87,8 @@
                   <strong>April</strong>
                   <span>18</span>
                 </time>
-                <span class="event-xs hidden-md hidden-lg">Cycle 2017-02 Wk3</span>
-                <span class="event hidden-xs hidden-sm">Cycle 2017-02 Wk3</span>
+                <span class="event-xs hidden-md hidden-lg">Cycle 2017-02 Wk3 of 4</span>
+                <span class="event hidden-xs hidden-sm">Cycle 2017-02 Wk3 of 4</span>
             </li>
             <li class="list-group-item">
                 <time datetime="2017-03-07" class="icon">
@@ -96,12 +96,11 @@
                   <strong>April</strong>
                   <span>25</span>
                 </time>
-                <span class="event-xs hidden-md hidden-lg">Cycle 2017-02 Wk4</span>
-                <span class="event hidden-xs hidden-sm">Cycle 2017-02 Wk4</span>
+                <span class="event-xs hidden-md hidden-lg">Cycle 2017-02 Wk4 of 4</span>
+                <span class="event hidden-xs hidden-sm">Cycle 2017-02 Wk4 of 4</span>
             </li>
             <li class="list-group-item list-group-item-info text-center">
                 <h5>May - Sept 2017 - TBA</h5>
             </li>
+          </div>
         </ul>
-    </div>
-</div>
