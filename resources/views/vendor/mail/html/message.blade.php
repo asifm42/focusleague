bcefe1e85f90d7b02448880a6e7b05299d5e77af
Asif Mohammed <asifm42@gmail.com>
2017-02-16 10:44:07 -0600

    adding vendor views for customization

---
+++ resources/views/vendor/mail/html/message.blade.php
@@ -0,0 +1,28 @@
+@component('mail::layout')
+    {{-- Header --}}
+    @slot('header')
+        @component('mail::header', ['url' => config('app.url')])
+            {{-- config('app.name') --}}
+            <img alt="FOCUS League" src="{{isset($message) ? $message->embed(public_path('assets/img/logo.png')) : url('assets/img/logo.png') }}" height="68" width="190" style="margin:10px; height:68px; width:190px;"/>
+        @endcomponent
+    @endslot
+
+    {{-- Body --}}
+    {{ $slot }}
+
+    {{-- Subcopy --}}
+    @if (isset($subcopy))
+        @slot('subcopy')
+            @component('mail::subcopy')
+                {{ $subcopy }}
+            @endcomponent
+        @endslot
+    @endif
+
+    {{-- Footer --}}
+    @slot('footer')
+        @component('mail::footer')
+            &copy; {{ date('Y') }} {{ config('app.name') }}. All rights reserved.
+        @endcomponent
+    @endslot
+@endcomponent
