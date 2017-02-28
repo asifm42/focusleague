695acd93fe93e148badc418b91c6b234e98ff38a
Asif Mohammed <asifm42@gmail.com>
2017-02-28 10:47:24 -0600

    removing dates that auto casted

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -14,7 +14,7 @@ class Game extends Model
      *
      * @var array
      */
-    protected $dates = ['starts_at', 'ends_at', 'created_at', 'updated_at', 'deleted_at'];
+    protected $dates = ['starts_at', 'ends_at', 'deleted_at'];
 
     /**
      * The attributes that are mass assignable.
