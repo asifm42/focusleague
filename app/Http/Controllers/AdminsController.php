987be4e73ab6cde7f642fdf8a7bea3db2b95f0dc
Asif Mohammed <asifm42@gmail.com>
2017-03-01 01:55:24 -0600

    commenting out debugbar

--- app/Http/Controllers/AdminsController.php
+++ app/Http/Controllers/AdminsController.php
@@ -17,7 +17,7 @@ class AdminsController extends Controller
         $data['user'] = $user;
         $cycle = Cycle::current_cycle();
         $cycle->load('teams', 'teams.captains');
-                         \Debugbar::info($cycle->teams);
+                         // \Debugbar::info($cycle->teams);
         $data['current_cycle'] = $cycle;
         $data['current_cycle_signups'] = $data['current_cycle']->signups()->get()->load('availability');
 
