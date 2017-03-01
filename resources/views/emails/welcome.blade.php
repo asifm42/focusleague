e35ea274ef82e50c8109c221976cde7f0ed930fe
Asif Mohammed <asifm42@gmail.com>
2017-02-28 19:34:31 -0600

    adding a button for sharing ultimate history

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -3,6 +3,10 @@
 
 <p>You can view your info and current cycle details at <a href="{{ route('users.dashboard') }}">your dashboard</a>. BUT before you can do that, we need you <a href="{{ route('ultimate_history.create') }}">share your ultimate history with us.</a></p>
 
+@component('mail::button', ['url' => route('ultimate_history.create'), 'color' => 'green'])
+SHARE YOUR ULTIMATE HISTORY
+@endcomponent
+
 <p>Make sure to check out the <a href="{{ route('site.faq') }}">FAQ page</a> so there are no surprises.</p>
 
 <p>Should you ever encounter problems with your account or forget your password, we will contact you at this address.</p>
