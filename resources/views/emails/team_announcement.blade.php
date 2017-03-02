34d876a3d454d297636f52f39e75f4c249d5d238
Asif Mohammed <asifm42@gmail.com>
2017-03-01 20:59:33 -0600

    updated text

--- resources/views/emails/team_announcement.blade.php
+++ resources/views/emails/team_announcement.blade.php
@@ -1,4 +1,4 @@
-@component('emails.layouts.message')
+@component('emails.layouts.message', ['user'=>$user])
 <p>Cycle {{ $cycle->name }} teams have been set!</p>
 
 <p>The format for the cycle is {{ $cycle->format }}.<p>
@@ -6,24 +6,26 @@
 <p>You are on team <strong><em>{{ ucwords($team->name) }}</em></strong> in the {{ ucfirst($team->division) }} division.</p>
 
 @if ($team->captains->count() > 1)
-<p>Your captains are:</p>
-
-<ul>
-@foreach($team->captains as $captain)
-<li>
-@if ($captain->user->nickname)
-{{ ucwords($captain->user->name) . ' aka ' . ucwords($captain->user->nickname) }}
-@else
-{{ ucwords($captain->user->name) }}
-@endif
-{{' - ' . $captain->user->email }}
-</li>
-@endforeach
-</ul>
+    <p>Your captains are:</p>
 @elseif (count($team->captains) == 1)
-<p>Your captain is {{ $team->captains->first()->user->name }} and can be reached at {{ $team->captains->first()->user->email }}.</p>
+    <p>Your captain is:</p>
 @else
-<p>We are working on selecting a captain since no one volunteered. Please let us know if you have changed your mind and are willing to captain this cycle.</p>
+    <p>We are working on selecting a captain since no one volunteered. Please let us know if you have changed your mind and are willing to captain this cycle.</p>
+@endif
+
+@if ($team->captains->count() > 0)
+<ul>
+    @foreach($team->captains as $captain)
+        <li>
+        @if ($captain->user->nickname)
+            {{ ucwords($captain->user->name) . ' aka ' . ucwords($captain->user->nickname) }}
+        @else
+            {{ ucwords($captain->user->name) }}
+        @endif
+            {{' - ' . $captain->user->email }}
+        </li>
+    @endforeach
+</ul>
 @endif
 
 <p>Your fee for this cycle is ${{ $cost }}.00. Your current balance is {{ $user->getBalanceString() }}. Please use one of the following methods of payment (listed in order of preference). Please put "Cycle {{ $cycle->name }} fees" in the note if possible.</p>
