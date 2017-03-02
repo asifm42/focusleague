66f1218b687e4806f58ee2f66afd14df9c5400d3
Asif Mohammed <asifm42@gmail.com>
2017-03-01 23:19:12 -0600

    code cleanup

--- app/Listeners/SendSubSignupAlert.php
+++ app/Listeners/SendSubSignupAlert.php
@@ -30,22 +30,5 @@ class SendSubSignupAlert
     public function handle(UserSignedUpAsASub $event)
     {
         $this->mailer->sendSubSignupAlert($event->sub);
-return;
-        $data=[];
-        $data['user'] = $event->user->toArray();
-        $data['date'] = $event->week->starts_at->toDateTimeString();
-
-        $sub = $event->week->subs->find($event->user->id);
-        $data['note'] = $sub->pivot->note;
-        $data['sub_created_at'] = $sub->pivot->created_at->toDateTimeString();
-
-        // Alert email, if you want to be notified upon new registrations
-        Mail::queue(['text' => 'emails.alert.subSignup'], $data, function($message)
-        {
-            $message->to('asifm42@gmail.com', 'Asif Mohammed')
-                    ->cc('gizmolito@gmail.com', 'Nicholas Carranza')
-                    ->from('system@focusleague.com', 'FOCUS League System')
-                    ->subject('New sub sign-up alert');
-        });
     }
 }
