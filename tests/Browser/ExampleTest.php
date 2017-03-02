a64d22b0846e069ccfe96d37d1b95cd1546e70dc
Asif Mohammed <asifm42@gmail.com>
2017-03-02 14:58:45 -0600

    don't think we can do database setup...keeps failing

--- tests/Browser/ExampleTest.php
+++ tests/Browser/ExampleTest.php
@@ -2,12 +2,21 @@
 
 namespace Tests\Browser;
 
-use Tests\DuskTestCase;
-use Laravel\Dusk\Browser;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+use Tests\Helpers\DatabaseSetup;
 
 class ExampleTest extends DuskTestCase
 {
+    use DatabaseMigrations;
+
+    protected function setup()
+    {
+        parent::setup();
+        // $this->setupDatabase();
+    }
+
     /**
      * A basic browser test example.
      *
@@ -18,6 +27,22 @@ class ExampleTest extends DuskTestCase
         $this->browse(function (Browser $browser) {
             $browser->visit('/')
                     ->assertSee('Laravel');
+        });
+    }
+
+    /**
+     * A basic browser test example.
+     *
+     * @return void
+     */
+    public function testBasicExample2()
+    {
+        factory(\App\Models\User::class)->create();
+
+        $this->browse(function (Browser $browser) {
+            $browser->loginAs(1)
+                    ->visit('/cycles/current')
+                    ->assertSee('Sign in');
         });
     }
 }
