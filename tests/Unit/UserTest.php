fcf7e152a5f885c49476b22891f6f6e97c7e16a5
Asif Mohammed <asifm42@gmail.com>
2017-03-08 13:57:16 -0600

    was declaring incorrect class name

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -10,7 +10,7 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
 
-class CycleTest extends TestCase
+class UserTest extends TestCase
 {
     use DatabaseMigrations;
 
