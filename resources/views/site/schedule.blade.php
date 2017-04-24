bc035efc91f44b72cc10fc5ab0e8d1f2b7f2afdc
Asif Mohammed <asifm42@gmail.com>
2017-04-24 10:14:11 -0500

    may-june schedule

--- resources/views/site/schedule.blade.php
+++ resources/views/site/schedule.blade.php
@@ -49,8 +49,88 @@
                 </time>
                 <div class="event">Cycle 2017-02 Wk4 of 4</div>
             </li>
+            <li class="list-group-item list-group-item-info">
+                <time datetime="2017-04-26" class="icon">
+                  <em>Wednesday</em>
+                  <strong>April</strong>
+                  <span>26</span>
+                </time>
+                <div class="event">Cycle&nbsp;2017-03 Registration&nbsp;Opens</div>
+            </li>
+            <li class="list-group-item">
+                <time datetime="2017-05-02" class="icon">
+                  <em>Tuesday</em>
+                  <strong>May</strong>
+                  <span>2</span>
+                </time>
+                <div class="event">Cycle 2017-03 Wk1 of 4</div>
+            </li>
+            <li class="list-group-item">
+                <time datetime="2017-05-09" class="icon">
+                  <em>Tuesday</em>
+                  <strong>May</strong>
+                  <span>9</span>
+                </time>
+                <div class="event">Cycle 2017-03 Wk2 of 4</div>
+            </li>
+            <li class="list-group-item">
+                <time datetime="2017-05-16" class="icon">
+                  <em>Tuesday</em>
+                  <strong>May</strong>
+                  <span>16</span>
+                </time>
+                <div class="event">Cycle 2017-03 Wk3 of 4</div>
+            </li>
+            <li class="list-group-item">
+                <time datetime="2017-05-23" class="icon">
+                  <em>Tuesday</em>
+                  <strong>May</strong>
+                  <span>23</span>
+                </time>
+                <div class="event">Cycle 2017-03 Wk4 of 4</div>
+            </li>
+            <li class="list-group-item list-group-item-info">
+                <time datetime="2017-05-24" class="icon">
+                  <em>Wednesday</em>
+                  <strong>May</strong>
+                  <span>24</span>
+                </time>
+                <div class="event">Cycle&nbsp;2017-03 Registration&nbsp;Opens</div>
+            </li>
+            <li class="list-group-item">
+                <time datetime="2017-05-30" class="icon">
+                  <em>Tuesday</em>
+                  <strong>May</strong>
+                  <span>30</span>
+                </time>
+                <div class="event">Cycle 2017-04 Wk1 of 4</div>
+            </li>
+            <li class="list-group-item">
+                <time datetime="2017-06-06" class="icon">
+                  <em>Tuesday</em>
+                  <strong>June</strong>
+                  <span>6</span>
+                </time>
+                <div class="event">Cycle 2017-04 Wk2 of 4</div>
+            </li>
+            <li class="list-group-item">
+                <time datetime="2017-06-13" class="icon">
+                  <em>Tuesday</em>
+                  <strong>June</strong>
+                  <span>16</span>
+                </time>
+                <div class="event">Cycle 2017-04 Wk3 of 4</div>
+            </li>
+            <li class="list-group-item">
+                <time datetime="2017-06-20" class="icon">
+                  <em>Tuesday</em>
+                  <strong>June</strong>
+                  <span>20</span>
+                </time>
+                <div class="event">Cycle 2017-04 Wk4 of 4</div>
+            </li>
             <li class="list-group-item list-group-item-info text-center">
-                <h5>May - Sept 2017 - TBA</h5>
+                <h5>July - Sept 2017 - TBA</h5>
             </li>
           </div>
         </ul>
