4f3080344ef8aab777baf270234957013c903167
Asif Mohammed <asifm42@gmail.com>
2017-05-17 15:58:48 -0500

    datatables on users and deliquents list

--- resources/views/transactions/index.blade.php
+++ resources/views/transactions/index.blade.php
@@ -10,69 +10,112 @@
         </div>
     </div>
     <div class="container">
+        @if (auth()->user()->isAdmin())
+            <div class="row">
+                <div class="col-xs-12 col-md-8">
+                        <h5>
+                            <a href="{{ route('users.show', $user->id) }}">{{ $user->name }} ({{ $user->getNicknameOrShortname() }})</a>
+                            <a href="{{ route('transactions.create') . '?user_id=' . $user->id }}" class="btn btn-default btn-xs pull-right">Add Transaction</a>
+                        </h5>
+                </div>
+            </div>
+        @endif
+        <div class="row">
+            <div class="col-xs-12 col-md-8">
+                <h5>
+                    @if ($balance < 0 )
+                        Credit: <span class="text-primary">${{ $balance }}</span>
+                    @elseif ($balance == 0 )
+                        Balance: <span>${{ $balance }}</span>
+                    @else
+                        Amount owed: <span class="text-danger">${{ $balance }}</span>
+                    @endif
+                </h5>
+            </div>
+        </div>
         <div class="row">
             <div class="col-xs-12 col-md-8">
                 <div class = "table-responsive">
-                    <table class="table table-striped table-condensed table-bordered">
-                        @if (auth()->user()->isAdmin())
+                    <table class="table table-striped table-condensed table-bordered focus-transactions-table">
+                        <thead>
                             <tr>
-                                <th colspan=8>Balance Details for <a href="{{ route('users.show', $user->id) }}">{{ $user->name }} ({{ $user->getNicknameOrShortname() }})</a></th>
-                            </tr>
-                        @endif
-                        <tr>
-                            <th>Date</th>
-                            <th>Cycle</th>
-                            <th>Week</th>
-                            <th>Description</th>
-                            <th>Type</th>
-                            <th>Method</th>
-                            <th class="text-right">Amount</th>
-                            @if (auth()->user()->isAdmin())
-                                <th>Admin</th>
-                            @endif
-                        </tr>
-                        @foreach ($transactions as $transaction)
-                            <tr>
-                                <td>{{ $transaction->date }}</td>
-                                <td>{{ $transaction->cycle ? $transaction->cycle->name : 'N/A'}}</td>
-                                <td>{{ $transaction->week ? $transaction->week->starts_at->toFormattedDateString() : 'N/A'}}</td>
-                                <td>{{ $transaction->description }}</td>
-                                <td>{{ ucwords($transaction->type) }}</td>
-                                <td>{{ $transaction->payment_type ? ucwords($transaction->payment_type) : 'N/A' }}</td>
-                                @if ($transaction->type === 'payment' || $transaction->type === 'credit')
-                                    <td class="text-right">-${{ number_format($transaction->amount, 2, '.', '') }}</td>
-                                @else
-                                    <td class="text-right text-danger">${{ number_format($transaction->amount, 2, '.', '') }}</td>
-                                @endif
+                                <th>Date</th>
+                                <th>Cycle</th>
+                                <th>Week</th>
+                                <th>Description</th>
+                                <th>Type</th>
+                                <th>Method</th>
+                                <th class="text-right">Amount</th>
                                 @if (auth()->user()->isAdmin())
-                                    <td class="text-center">
-                                        <a href="{{ route('transactions.edit', $transaction->id) }}" class="btn btn-default btn-xs">Edit</a>
-                                    </td>
+                                    <th>Admin</th>
                                 @endif
                             </tr>
-                        @endforeach
-                        <tr>
-                            <tr>
-                                @if ($balance < 0 )
-                                    <td colspan="6" class="text-right">Credit</td>
-                                    <td class="text-right text-primary">${{ $balance }}
-                                @elseif ($balance == 0 )
-                                    <td colspan="6" class="text-right">Balance</td>
-                                    <td class="text-right">${{ $balance }}
-                                @else
-                                    <td colspan="6" class="text-right">Amount owed</td>
-                                    <td class="text-right text-danger">${{ $balance }}
-                                @endif
-                                @if (auth()->user()->isAdmin())
-                                    <td class="text-center">
-                                        <a href="{{ route('transactions.create') . '?user_id=' . $user->id }}" class="btn btn-default btn-xs">Add</a>
-                                    </td>
-                                @endif
-                            </tr>
-                        </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($transactions as $transaction)
+                                <tr>
+                                    <td>{{ $transaction->date }}</td>
+                                    <td>{{ $transaction->cycle ? $transaction->cycle->name : 'N/A'}}</td>
+                                    <td>{{ $transaction->week ? $transaction->week->starts_at->toFormattedDateString() : 'N/A'}}</td>
+                                    <td>{{ $transaction->description }}</td>
+                                    <td>{{ ucwords($transaction->type) }}</td>
+                                    <td>{{ $transaction->payment_type ? ucwords($transaction->payment_type) : 'N/A' }}</td>
+                                    @if ($transaction->type === 'payment' || $transaction->type === 'credit')
+                                        <td class="text-right">-${{ number_format($transaction->amount, 2, '.', '') }}</td>
+                                    @else
+                                        <td class="text-right text-danger">${{ number_format($transaction->amount, 2, '.', '') }}</td>
+                                    @endif
+                                    @if (auth()->user()->isAdmin())
+                                        <td class="text-center">
+                                            <a href="{{ route('transactions.edit', $transaction->id) }}" class="btn btn-default btn-xs">Edit</a>
+                                        </td>
+                                    @endif
+                                </tr>
+                            @endforeach
+                        </tbody>
                     </table>
                 </div>
             </div>
         </div>
+
+        @if (auth()->user()->isAdmin())
+            <div class="col-xs-12 col-md-8">
+                <h5 class="pull-right">
+                    @if ($balance < 0 )
+                        Credit: <span class="text-primary">${{ $balance }}</span>
+                    @elseif ($balance == 0 )
+                        Balance: <span>${{ $balance }}</span>
+                    @else
+                        Amount owed: <span class="text-danger">${{ $balance }}</span>
+                    @endif
+                </h5>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-md-8">
+                        <h5>
+                            <a href="{{ route('users.show', $user->id) }}">{{ $user->name }} ({{ $user->getNicknameOrShortname() }})</a>
+                            <a href="{{ route('transactions.create') . '?user_id=' . $user->id }}" class="btn btn-default btn-xs pull-right">Add Transaction</a>
+                        </h5>
+                </div>
+            </div>
+        @endif
     </div>
 @endsection
+@push('scripts')
+  <script>
+    $(document).ready(function(){
+
+      $('.focus-transactions-table').DataTable({
+        "dom": '<"toolbar">frtip',
+        "language": {
+          "info": "Showing _START_ to _END_ of _TOTAL_ transactions."
+        },
+        "paging":    false,
+        "searching": true
+      });
+
+
+
+    });
+  </script>
+@endpush
