5d9d1c0efe5ee95648fe7896d9ffc0eb292935ab
Asif Mohammed <asifm42@gmail.com>
2018-03-16 10:56:10 -0500

    Update for bootstrap 4 support

--- resources/views/admin/cycle_details.blade.php
+++ resources/views/admin/cycle_details.blade.php
@@ -2,20 +2,18 @@
 @section('title','FOCUS League – Admin Cycle Overview')
 
 @section('content')
-    <div class="page-header">
-        <div class="container">
-            <h4 class="hidden-md hidden-lg">Admin Cycle Overview</h4>
-            <h3 class="hidden-xs hidden-sm">Admin Cycle Overview</h3>
-            <p>Overview of the cycle</p>
-        </div>
-    </div>
     <div class="container">
         <div class="row">
-            <div class="col-xs-12 col-md-6">
+            <div class="col">
+                <h4 class="">Admin Cycle Overview</h4>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col col-md-6">
             @if(!empty($cycle))
-                <div class="panel panel-default">
-                    <div class="panel-heading">Cycle Details</div>
-                    <div class="panel-body">
+                <div class="card">
+                    <div class="card-header">Cycle Details</div>
+                    <div class="card-body">
                         <dl class="horizontal">
                             <dt>Name</dt>
                             <dd>{{ $cycle->name }}</dd>
@@ -43,14 +41,14 @@
 
 
         <div class="row">
-            <div class="col-xs-12 col-md-4">
-            @include('signups.panel', $data = ['signups'=>$mensOnly, 'cycle'=>$cycle, 'title' => 'Mens only'])
+            <div class="col col-md-4">
+            @include('signups.card', $data = ['signups'=>$mensOnly, 'cycle'=>$cycle, 'title' => 'Mens only'])
             </div>
-            <div class="col-xs-12 col-md-4">
-            @include('signups.panel', $data = ['signups'=>$mensFlexible, 'cycle'=>$cycle, 'title' => 'Mens flexible'])
+            <div class="col col-md-4">
+            @include('signups.card', $data = ['signups'=>$mensFlexible, 'cycle'=>$cycle, 'title' => 'Mens flexible'])
             </div>
-            <div class="col-xs-12 col-md-4">
-            @include('signups.panel', $data = ['signups'=>$mixedFlexibleMale, 'cycle'=>$cycle, 'title' => 'Mixed flexible - male'])
+            <div class="col col-md-4">
+            @include('signups.card', $data = ['signups'=>$mixedFlexibleMale, 'cycle'=>$cycle, 'title' => 'Mixed flexible - male'])
             </div>
         </div>
 
@@ -58,23 +56,23 @@
 
         <div class="row">
 
-            <div class="col-xs-12 col-md-4">
-            @include('signups.panel', $data = ['signups'=>$womensOnly, 'cycle'=>$cycle, 'title' => 'Womens only'])
+            <div class="col col-md-4">
+            @include('signups.card', $data = ['signups'=>$womensOnly, 'cycle'=>$cycle, 'title' => 'Womens only'])
             </div>
-            <div class="col-xs-12 col-md-4">
-            @include('signups.panel', $data = ['signups'=>$womensFlexible, 'cycle'=>$cycle, 'title' => 'Womens flexible'])
+            <div class="col col-md-4">
+            @include('signups.card', $data = ['signups'=>$womensFlexible, 'cycle'=>$cycle, 'title' => 'Womens flexible'])
             </div>
-            <div class="col-xs-12 col-md-4">
-            @include('signups.panel', $data = ['signups'=>$mixedFlexibleFemale, 'cycle'=>$cycle, 'title' => 'Mixed flexible - female'])
+            <div class="col col-md-4">
+            @include('signups.card', $data = ['signups'=>$mixedFlexibleFemale, 'cycle'=>$cycle, 'title' => 'Mixed flexible - female'])
             </div>
 
         </div>
         <div class="row">
-            <div class="col-xs-12 col-md-4">
-            @include('signups.panel', $data = ['signups'=>$mixedOnlyMale, 'cycle'=>$cycle, 'title' => 'Mixed only - male'])
+            <div class="col col-md-4">
+            @include('signups.card', $data = ['signups'=>$mixedOnlyMale, 'cycle'=>$cycle, 'title' => 'Mixed only - male'])
             </div>
-            <div class="col-xs-12 col-md-4">
-            @include('signups.panel', $data = ['signups'=>$mixedOnlyFemale, 'cycle'=>$cycle, 'title' => 'Mixed only - female'])
+            <div class="col col-md-4">
+            @include('signups.card', $data = ['signups'=>$mixedOnlyFemale, 'cycle'=>$cycle, 'title' => 'Mixed only - female'])
             </div>
         </div>
     </div>
