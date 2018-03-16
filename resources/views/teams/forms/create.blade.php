d6f9253806983db266223ac4665d2b22c80de512
Asif Mohammed <asifm42@gmail.com>
2018-03-16 13:42:30 -0500

    Nitpicking

--- resources/views/teams/forms/create.blade.php
+++ resources/views/teams/forms/create.blade.php
@@ -13,37 +13,39 @@
         {!! method_field('patch') !!}
     @endif
 
-<div class="card">
+    <div class="card">
+        <div class="card-body">
+            <div class="form-group {{ $errors->has('name') ? 'has-danger' : ''}}">
+                <label for="name" class="required">Team Name</label>
 
-    <div class="card-body">
-        <div class="form-group {{ $errors->has('name') ? 'has-danger' : ''}}">
-            <label for="name" class="required">Team Name</label>
-            <input name="name" type="text" class="form-control {{ $errors->has('name') ? 'is-invalid' : ''}}" id="name" aria-describedby="nameHelp" placeholder="Required team name" required value={{ old('name', $team->name) }}>
-            <div id="nameFeedback" class="invalid-feedback">{{ $errors->has('name') ? $errors->first('name') : '' }}</div>
+                <input name="name" type="text" class="form-control {{ $errors->has('name') ? 'is-invalid' : ''}}" id="name" aria-describedby="nameHelp" placeholder="Required team name" required value={{ old('name', $team->name) }}>
+
+                <div id="nameFeedback" class="invalid-feedback">{{ $errors->has('name') ? $errors->first('name') : '' }}</div>
+            </div>
+
+            @php
+                $divOptions = ['mens' => 'Mens', 'mixed' => 'Mixed', 'womens' => 'Womens'];
+            @endphp
+
+            <div class="form-group {{ $errors->has('division') ? 'has-danger' : ''}}">
+                <label for="division" class="required">Division</label>
+
+                <select name="division" class="form-control {{ $errors->has('division') ? 'is-invalid' : ''}}" id="division" aria-describedby="divisionHelp" placeholder="Required division" required>
+                    <option disabled {{ old('division') ? '' : 'selected' }}>Required division</option>
+                    @foreach($divOptions as $key => $option)
+                    <option value="{{ $key }}" {{ old('division', $team->division) == $key ? 'selected' : '' }}>{{ $option }}</option>
+                    @endforeach
+                </select>
+
+                <div id="divisionFeedback" class="invalid-feedback">{{ $errors->has('division') ? $errors->first('division') : '' }}</div>
+            </div>
+
+            <input class="form-control" type="hidden" name="cycle_id" value="{{ $cycle->id }}">
+
         </div>
-
-        @php
-            $divOptions = ['mens' => 'Mens', 'mixed' => 'Mixed', 'womens' => 'Womens'];
-        @endphp
-
-        <div class="form-group {{ $errors->has('division') ? 'has-danger' : ''}}">
-            <label for="division" class="required">Division</label>
-            <select name="division" class="form-control {{ $errors->has('division') ? 'is-invalid' : ''}}" id="division" aria-describedby="divisionHelp" placeholder="Required division" required>
-                <option disabled {{ old('division') ? '' : 'selected' }}>Required division</option>
-                @foreach($divOptions as $key => $option)
-                <option value="{{ $key }}" {{ old('division', $team->division) == $key ? 'selected' : '' }}>{{ $option }}</option>
-                @endforeach
-            </select>
-            <div id="divisionFeedback" class="invalid-feedback">{{ $errors->has('division') ? $errors->first('division') : '' }}</div>
-        </div>
-
-
-        <input class="form-control" type="hidden" name="cycle_id" value="{{ $cycle->id }}">
 
     </div>
-
-</div>
-        @if($edit === true)
+    @if($edit === true)
         <div class="row mt-3">
             <div class="col">
                     <input class="btn btn btn-primary" type="submit" value="Save" >
@@ -59,19 +61,8 @@
                 </form>
             </div>
         </div>
-        @else
-
-                <input class="btn btn btn-primary btn-block mt-3" type="submit" value="Save" >
-                {{ csrf_field() }}
-            </form>
-        @endif
-
-@push('scripts')
-    <script>
-        $(document).ready( function () {
-            // For popovers on the navbar
-            // $('[data-toggle="popover"]').popover();
-
-        })
-    </script>
-@endpush
+    @else
+            <input class="btn btn btn-primary btn-block mt-3" type="submit" value="Save" >
+            {{ csrf_field() }}
+        </form>
+    @endif
