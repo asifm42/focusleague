ec90125bc4353abd7d292039a0a38ee50bfa2ec3
Asif Mohammed <asifm42@gmail.com>
2018-03-18 17:06:57 -0500

    Add blade component for time icon

---
+++ resources/views/site/calendar-icon.blade.php
@@ -0,0 +1,36 @@
+@if(!isset($class))
+    @php
+        $class = "";
+    @endphp
+@endif
+
+@if(isset($date))
+    @php
+        if (is_string($date)) {
+            $date = new Carbon\Carbon($date);
+        }
+
+        if (! (get_class($date) == 'Carbon\Carbon')) {
+            $date = new Carbon\Carbon($date);
+        }
+
+        $datetime = $date->format('Y-m-d');
+        $dayName = $date->format('l');
+        $month = $date->format('F');
+        $dayNumber = $date->format('j');
+    @endphp
+@endif
+
+@if(isset($color))
+    <time datetime="{{ $datetime }}" class="icon {{ $class }}">
+        <em style="color: {{ $color }}">{{ $dayName }}</em>
+        <strong style="background-color: {{ $color }}">{{ $month }}</strong>
+        <span>{{ $dayNumber }}</span>
+    </time>
+@else
+    <time datetime="{{ $datetime }}" class="icon {{ $class }}">
+        <em>{{ $dayName }}</em>
+        <strong>{{ $month }}</strong>
+        <span>{{ $dayNumber }}</span>
+    </time>
+@endif
