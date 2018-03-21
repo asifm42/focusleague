85c19af0747c094fafd4c931ebe019b4c15841a2
Asif Mohammed <asifm42@gmail.com>
2018-03-21 17:51:46 -0500

    Update laracasts flash to 3.0

--- resources/views/vendor/flash/message.blade.php
+++ resources/views/vendor/flash/message.blade.php
@@ -1,4 +1,4 @@
-@if (session()->has('flash_notification.message'))
+{{-- @if (session()->has('flash_notification.message'))
     @if (session()->has('flash_notification.overlay'))
         @include('flash::modal', [
             'modalClass'    => 'flash-modal',
@@ -14,4 +14,35 @@
             {!! session('flash_notification.message') !!}
         </div>
     @endif
-@endif
+@endif --}}
+
+
+
+@foreach (session('flash_notification', collect())->toArray() as $message)
+    @if ($message['overlay'])
+        @include('flash::modal', [
+            'modalClass' => 'flash-modal',
+            'title'      => $message['title'],
+            'body'       => $message['message']
+        ])
+    @else
+        <div class="alert
+                    alert-{{ $message['level'] }}
+                    {{ $message['important'] ? 'alert-important' : '' }}
+                    rounded-0"
+                    role="alert"
+        >
+            @if ($message['important'])
+                <button type="button"
+                        class="close"
+                        data-dismiss="alert"
+                        aria-hidden="true"
+                >&times;</button>
+            @endif
+
+            {!! $message['message'] !!}
+        </div>
+    @endif
+@endforeach
+
+{{ session()->forget('flash_notification') }}
