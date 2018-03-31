c34fde115c85edacc668e5599bba4bc4ec53f4c4
Asif Mohammed <asifm42@gmail.com>
2018-03-31 09:16:13 -0500

    Implement Laravel Mix versioning

--- webpack.mix.js
+++ webpack.mix.js
@@ -20,3 +20,8 @@ mix.js('resources/assets/js/app.js', 'public/js')
     })
    .sass('resources/assets/sass/app.scss', 'public/css')
    .sourceMaps();
+
+
+if (mix.inProduction()) {
+    mix.version();
+}
