322e7c800dacf4eab11c0892b6de061d22db720b
Asif Mohammed <asifm42@gmail.com>
2018-04-27 11:07:40 -0500

    update schedule for May 2018

--- resources/views/site/schedule.blade.php
+++ resources/views/site/schedule.blade.php
@@ -9,72 +9,77 @@
                 </h6>
             </li>
             <div class="schedule-list">
-            <li class="list-group-item list-group-item-info rounded-0 d-flex flex-row">
-                <time datetime="2018-03-14" class="icon">
-                  <em>Wednesday</em>
-                  <strong>March</strong>
-                  <span>14</span>
-                </time>
-                <div class="event m-auto">Cycle&nbsp;2018-01<br/>Registration&nbsp;Opens</div>
-            </li>
-            <li class="list-group-item d-flex flex-row">
-                <time datetime="2018-03-20" class="icon">
-                  <em>Tuesday</em>
-                  <strong>March</strong>
-                  <span>20</span>
-                </time>
-                <div class="event m-auto">Cycle&nbsp;2018-01 Wk1&nbsp;of&nbsp;3</div>
-            </li>
-            <li class="list-group-item d-flex flex-row">
-                <time datetime="2018-03-27" class="icon">
-                  <em>Tuesday</em>
-                  <strong>March</strong>
-                  <span>27</span>
-                </time>
-                <div class="event m-auto">Cycle&nbsp;2018-01 Wk2&nbsp;of&nbsp;3</div>
-            </li>
-            <li class="list-group-item d-flex flex-row">
-                <time datetime="2018-04-03" class="icon">
-                  <em>Tuesday</em>
-                  <strong>April</strong>
-                  <span>3</span>
-                </time>
-                <div class="event m-auto">Cycle&nbsp;2018-01 Wk3&nbsp;of&nbsp;3</div>
-            </li>
-            <li class="list-group-item list-group-item-info d-flex flex-row">
-                <time datetime="2018-04-04" class="icon">
-                  <em>Wednesday</em>
-                  <strong>April</strong>
-                  <span>4</span>
-                </time>
-                <div class="event m-auto">Cycle&nbsp;2018-02<br/>Registration&nbsp;Opens</div>
-            </li>
-            <li class="list-group-item d-flex flex-row">
-                <time datetime="2018-04-10" class="icon">
-                  <em>Tuesday</em>
-                  <strong>April</strong>
-                  <span>10</span>
-                </time>
-                <div class="event m-auto">Cycle&nbsp;2018-02 Wk1&nbsp;of&nbsp;3</div>
-            </li>
-            <li class="list-group-item d-flex flex-row">
-                <time datetime="2018-04-17" class="icon">
-                  <em>Tuesday</em>
-                  <strong>April</strong>
-                  <span>17</span>
-                </time>
-                <div class="event m-auto">Cycle&nbsp;2018-02 Wk2&nbsp;of&nbsp;3</div>
-            </li>
-            <li class="list-group-item d-flex flex-row">
-                <time datetime="2018-04-24" class="icon">
-                  <em>Tuesday</em>
-                  <strong>April</strong>
-                  <span>24</span>
-                </time>
-                <div class="event m-auto">Cycle&nbsp;2018-02 Wk3&nbsp;of&nbsp;3</div>
+              <div class="d-none">
+                <li class="list-group-item list-group-item-info rounded-0 d-flex flex-row">
+                    <time datetime="2018-03-14" class="icon">
+                      <em>Wednesday</em>
+                      <strong>March</strong>
+                      <span>14</span>
+                    </time>
+                    <div class="event m-auto">Cycle&nbsp;2018-01<br/>Registration&nbsp;Opens</div>
+                </li>
+                <li class="list-group-item d-flex flex-row">
+                    <time datetime="2018-03-20" class="icon">
+                      <em>Tuesday</em>
+                      <strong>March</strong>
+                      <span>20</span>
+                    </time>
+                    <div class="event m-auto">Cycle&nbsp;2018-01 Wk1&nbsp;of&nbsp;3</div>
+                </li>
+                <li class="list-group-item d-flex flex-row">
+                    <time datetime="2018-03-27" class="icon">
+                      <em>Tuesday</em>
+                      <strong>March</strong>
+                      <span>27</span>
+                    </time>
+                    <div class="event m-auto">Cycle&nbsp;2018-01 Wk2&nbsp;of&nbsp;3</div>
+                </li>
+                <li class="list-group-item d-flex flex-row">
+                    <time datetime="2018-04-03" class="icon">
+                      <em>Tuesday</em>
+                      <strong>April</strong>
+                      <span>3</span>
+                    </time>
+                    <div class="event m-auto">Cycle&nbsp;2018-01 Wk3&nbsp;of&nbsp;3</div>
+                </li>
+                <li class="list-group-item list-group-item-info d-flex flex-row">
+                    <time datetime="2018-04-04" class="icon">
+                      <em>Wednesday</em>
+                      <strong>April</strong>
+                      <span>4</span>
+                    </time>
+                    <div class="event m-auto">Cycle&nbsp;2018-02<br/>Registration&nbsp;Opens</div>
+                </li>
+                <li class="list-group-item d-flex flex-row">
+                    <time datetime="2018-04-10" class="icon">
+                      <em>Tuesday</em>
+                      <strong>April</strong>
+                      <span>10</span>
+                    </time>
+                    <div class="event m-auto">Cycle&nbsp;2018-02 Wk1&nbsp;of&nbsp;3</div>
+                </li>
+                <li class="list-group-item d-flex flex-row">
+                    <time datetime="2018-04-17" class="icon">
+                      <em>Tuesday</em>
+                      <strong>April</strong>
+                      <span>17</span>
+                    </time>
+                    <div class="event m-auto">Cycle&nbsp;2018-02 Wk2&nbsp;of&nbsp;3</div>
+                </li>
+                <li class="list-group-item d-flex flex-row">
+                    <time datetime="2018-04-24" class="icon">
+                      <em>Tuesday</em>
+                      <strong>April</strong>
+                      <span>24</span>
+                    </time>
+                    <div class="event m-auto">Cycle&nbsp;2018-02 Wk3&nbsp;of&nbsp;3</div>
+                </li>
+            </div>
+            <li class="list-group-item list-group-item-warning d-flex flex-row">
+                <h5 class="event m-auto">May 2018 - No Games</h5>
             </li>
             <li class="list-group-item list-group-item-info text-center">
-                <h5>May - Sept 2018 - TBA</h5>
+                <h5>June - Sept 2018 - TBA</h5>
             </li>
           </div>
         </ul>
