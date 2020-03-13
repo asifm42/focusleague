36e8850a4bfd59c9eb781406c000669eb360a10c
Asif Mohammed <asifm42@gmail.com>
2020-03-12 19:12:09 -0500

    Covid Announcement

---
+++ resources/views/emails/Covid_March12_Announcement.blade.php
@@ -0,0 +1,22 @@
+@component('emails.layouts.message', ['user' => $user ])
+<p>
+    Games are on for tonight! HSP is open.
+</p>
+
+<ul>
+    <li>Use your best judgement if you or a family member displays flu/cold like symptoms</li>
+    <li>Please be vigilant in washing your hands and following normal flu season precautions</li>
+    <li>We are instituting a precautionary no contact / no hand shake policy aligning with USSSA health & safety procedures.  This includes all physical contact and extends to customary Ultimate related activities, handshakes, fist bumps, and high-fives. Instead players should use verbal queues, without shaking hands, high-fiving or fist-bumping.</li>
+    <li>All coughing must be done into shirt and covered by both hands</li>
+</ul>
+
+<p>Communication from HSP:</p>
+<blockquote>Houston Sports Park is aware and following the COVID-19 updates from the City of Houston, Harris County and the Houston Health Department.  HSP is a public facility owned by the City of Houston, and the City’s declaration did not address public parks or other facilities, only events that are produced, co-sponsored, and permitted by the City of Houston.</blockquote>
+<blockquote>As such, we will remain open but we encourage everyone to use their best judgment based on the recommendations provided by the City, County, CDC and other experts. We have already increased the frequency of cleaning equipment, benches, restrooms, and vending machines.</blockquote>
+
+
+<p>
+    Help us spread the word! Tell your friends. Follow us on <a href="https://twitter.com/focusleague">Twitter</a>. Like us on <a href="https://facebook.com/focusleague">Facebook</a>.
+</p>
+<p>Lets play hard and lets play fair!</p>
+@endcomponent
