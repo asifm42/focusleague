91cdea61276a92b1289ccb2263243ff449c099d7
Asif Mohammed <asifm42@gmail.com>
2020-03-17 18:44:47 -0500

    add march 17 covid update view

---
+++ resources/views/emails/Covid_March17_Announcement.blade.php
@@ -0,0 +1,35 @@
+@component('emails.layouts.message', ['user' => $user ])
+<p>
+    If you are like me, your email inbox has been flooded by updates from companies and organizations on their response to COVID-19. Every day, some new sacrifice or temporary normality is asked of us. The next couple weeks or months are going to be difficult but the sooner we respond as a community the sooner this will pass.
+</p>
+<p>
+    To that end, FOCUS League is canceling this week and next week’s games. We will evaluate the dynamic situation in 2 weeks to determine Cycle 2’s status.
+</p>
+<p>
+    For players that were signed up to play this week and next week, we will treat the games as rainouts (as HSP is treating it) and will process credits accordingly. If the rest of the scheduled cycles are canceled, then we can discuss refunds on the credits if you don’t want to carry them into next year.
+</p>
+<p>
+    Some rationale for social distancing:
+</p>
+    <blockquote>"While most healthy people will recover from COVID-19, older adults and people with weakened immune systems are at increased risk of serious health consequences. If everyone avoids large gatherings, we will reduce the likelihood of young, healthy people giving their grandparents or immune-compromised friends a virus from which they may not recover."</blockquote>
+<p>
+    - Dr. David Persse, local health authority for the Houston Health Department
+</p>
+<p>
+    While COVID-19 is a new respiratory virus, daily precautions recommended to prevent respiratory illnesses are the same:
+</p>
+<ul>
+    <li>Wash hands frequently with soap and water for at least 20 seconds. Use hand sanitizer when you can't wash your hands.</li>
+    <li>Avoid touching your eyes, nose, and mouth.</li>
+    <li>Cover your coughs and sneezes with a tissue and throw the tissue away. If you don't have a tissue, use the elbow of your sleeve. Don't use your hands to cover coughs and sneezes.</li>
+    <li>Avoid close contact with people who are sick.</li>
+    <li>Stay home when you are sick and keep children home when they are sick.</li>
+    <li>Clean and disinfect frequently touched objects and surfaces.</li>
+    <li>Get a flu shot. (Although the flu shot does not protect against COVID-19, it is flu season.)</li>
+</ul>
+
+<p>
+    Help us spread the word! Tell your friends. Follow us on <a href="https://twitter.com/focusleague">Twitter</a>. Like us on <a href="https://facebook.com/focusleague">Facebook</a>.
+</p>
+
+@endcomponent
